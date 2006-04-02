927f4b8cf13a7db66aac870b7bf440cae861f692
Noel O'Boyle <baoilleach@gmail.com>
2006-04-02 21:31:12 +0000

    Starting to use proper unit testing using the unittest module

--- test/testall.py
+++ test/testall.py
@@ -1,27 +1,44 @@
-import os
-from cclib.parser import GAMESS,G03
+import os, unittest
+from cclib.parser import GAMESS,G03,ADF
+from Numeric import array
 
-os.chdir(os.path.join("..","data"))
+class GenericTest(unittest.TestCase):
+    def testhomos(self):
+        self.assertEquals(self.data.homos,array([34]))
 
-testfiles = [G03(os.path.join("Gaussian","basicGaussian03","dvb_gopt.out")),
-             GAMESS(os.path.join("GAMESS","basicPCGAMESS","dvb_gopt_a.out"))]
+    def testnatom(self):
+        self.assertEquals(self.data.natom,20)
 
-for testfile in testfiles:
-	testfile.logger.setLevel(0)
-	testfile.parse()
+    def testnbasis(self):
+        self.assertEquals(self.data.nbasis,60)
 
-attribs = ['natom','homos','nbasis']
-for attrib in attribs:
-	print attrib,
-	for testfile in testfiles:
-		print testfile.__getattribute__(attrib),
-	print
+    def testscfenergy(self):
+        self.assert_(self.data.scfenergies[-1]+382.3<3)
 
-print "Energy of optimised molecule",
-for testfile in testfiles:
-	print testfile.scfenergies[-1],
-print
-print "Energy of HOMO",
-for testfile in testfiles:
-	print testfile.moenergies[0,testfile.homos[0]],
-print
+    def testhomoenergy(self):
+        self.assert_(self.data.moenergies[0,self.data.homos[0]]+4.165<0.5,"HOMO energy is %f (for G03 it's -4.165)" % self.data.moenergies[0,self.data.homos[0]])
+
+class GaussianTest(GenericTest):
+    def setUp(self):
+        self.data = G03(os.path.join("..","data","Gaussian","basicGaussian03","dvb_gopt.out"))
+        self.data.logger.setLevel(0)
+        self.data.parse()
+
+class GamessTest(GenericTest):
+    def setUp(self):
+        self.data = GAMESS(os.path.join("..","data","GAMESS","basicPCGAMESS","dvb_gopt_a.out"))
+        self.data.logger.setLevel(0)
+        self.data.parse()
+
+class ADFTest(GenericTest):
+    def setUp(self):
+        self.data = ADF(os.path.join("..","data","ADF","basicADF2004.01","dvb_gopt.adfout"))
+        self.data.logger.setLevel(0)
+        self.data.parse()
+    
+if __name__=="__main__":
+    gaussiantests = unittest.makeSuite(GaussianTest)
+    gamesstests = unittest.makeSuite(GamessTest)
+    adftests = unittest.makeSuite(ADFTest)
+    alltests = unittest.TestSuite((gaussiantests,gamesstests,adftests))
+    unittest.TextTestRunner(verbosity=2).run(alltests)
