f6375d5228391328e8c2b4d466ddeb0b8cc41efd
Noel O'Boyle <baoilleach@gmail.com>
2006-04-04 16:06:01 +0000

    Added tests for initial GAMESS-US calculations (on Linux).

--- test/testall.py
+++ test/testall.py
@@ -23,7 +23,11 @@ class GaussianGeoOptTest(GenericGeoOptTest):
     def setUp(self):
         self.data = getfile(G03,"basicGaussian03","dvb_gopt.out")
 
-class GamessGeoOptTest(GenericGeoOptTest):
+class GamessUSGeoOptTest(GenericGeoOptTest):
+    def setUp(self):
+        self.data = getfile(GAMESS,"basicGAMESS-US","dvb_gopt_a.out")
+
+class PCGamessGeoOptTest(GenericGeoOptTest):
     def setUp(self):
         self.data = getfile(GAMESS,"basicPCGAMESS","dvb_gopt_a.out")
 
@@ -46,23 +50,30 @@ def visualtests():
     """These are not formal tests -- but they should be eyeballed."""
     logfiles = [ getfile(G03,"basicGaussian03","dvb_gopt.out"),
                  getfile(GAMESS,"basicPCGAMESS","dvb_gopt_a.out"),
+                 getfile(GAMESS,"basicGAMESS-US","dvb_gopt_a.out"),
                  getfile(ADF,"basicADF2004.01","dvb_gopt.adfout") ]
 
     print "\n\nMO energies of optimised dvb"
-    print "    ","".join(["%7s" % x for x in ['Gaussian','GAMESS','ADF']])
-    print "HOMO", " ".join(["%+2.4f" % x.moenergies[0,x.homos[0]] for x in logfiles])
-    print "LUMO", " ".join(["%+2.4f" % x.moenergies[0,x.homos[0]+1] for x in logfiles])
-    print "H-L  ", " ".join(["%2.4f" % (x.moenergies[0,x.homos[0]+1]-x.moenergies[0,x.homos[0]],) for x in logfiles])
+    print "    ","".join(["%8s" % x for x in ['Gaussian','PCGAMESS','GAMESS-US','ADF']])
+    print "HOMO", "  ".join(["%+2.4f" % x.moenergies[0,x.homos[0]] for x in logfiles])
+    print "LUMO", "  ".join(["%+2.4f" % x.moenergies[0,x.homos[0]+1] for x in logfiles])
+    print "H-L  ", "  ".join(["%2.4f" % (x.moenergies[0,x.homos[0]+1]-x.moenergies[0,x.homos[0]],) for x in logfiles])
+
+    for x in logfiles:
+        print x.mosyms
     
     
 if __name__=="__main__":
     gaussiantests = unittest.makeSuite(GaussianGeoOptTest)
-    gamesstests = unittest.makeSuite(GamessGeoOptTest)
+    pcgamesstests = unittest.makeSuite(PCGamessGeoOptTest)
+    gamessustests = unittest.makeSuite(GamessUSGeoOptTest)
     adftests = unittest.makeSuite(ADFGeoOptTest)
     print "\n*** Testing Gaussian dvb_gopt.out ***"
     unittest.TextTestRunner(verbosity=2).run(gaussiantests)
-    print "\n\n*** Testing GAMESS dvb_gopt_a.out ***"    
-    unittest.TextTestRunner(verbosity=2).run(gamesstests)
+    print "\n\n*** Testing PCGAMESS dvb_gopt_a.out ***"    
+    unittest.TextTestRunner(verbosity=2).run(pcgamesstests)
+    print "\n\n*** Testing GAMESS-US dvb_gopt_a.out ***"    
+    unittest.TextTestRunner(verbosity=2).run(gamessustests)
     print "\n\n*** Testing ADF dvb_gopt.adfout ***"
     unittest.TextTestRunner(verbosity=2).run(adftests)
     print "\n\n*** Visual tests ***"
