86ac48a3dcb71a4e504dcf04e30a00262d168e5f
Noel O'Boyle <baoilleach@gmail.com>
2006-04-17 20:43:24 +0000

    Test the parsing of all of the uploaded Jaguar files

--- test/parseJaguar.py
+++ test/parseJaguar.py
@@ -3,12 +3,13 @@ from cclib.parser import Jaguar
 
 os.chdir(os.path.join("..","data","Jaguar","basicJaguar"))
 
-os.chdir("eg01")
+files = [ ["eg01","dvb_gopt.out"],
+          ["eg02","dvb_sp.out"],
+          ["eg03","dvb_ir.out"],
+          ["eg06","dvb_un_sp.out"] ]
 
-for file in ["dvb_gopt.out"]:
-	t = Jaguar(file)
-	t.parse()
-
-print t.moenergies[0,:]
-print t.homos[0]
-print t.moenergies[0,t.homos[0]]
+for f in files:
+    t = Jaguar(os.path.join(f[0],f[1]))
+    t.parse()
+    if f[0]!="eg03":
+        print t.scfvalues
