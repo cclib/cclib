5482e5a2a3e7a67c506fb194ff9dff3603a5c136
Noel O'Boyle <baoilleach@gmail.com>
2006-04-22 17:39:59 +0000

    Adding a script to parse ADF files

---
+++ test/parseADF.py
@@ -0,0 +1,12 @@
+import os
+from cclib.parser import ADF
+
+os.chdir(os.path.join("..","data","ADF"))
+
+os.chdir("basicADF2004.01")
+
+for file in ["dvb_gopt.adfout"]:
+    t = ADF(file)
+    t.parse()
+
+
