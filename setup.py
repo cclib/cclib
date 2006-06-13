ce22361f31191f42733a434d81373f94462bd7da
Noel O'Boyle <baoilleach@gmail.com>
2006-06-13 14:44:22 +0000

    Added trove classifiers to setup.py. Also, realised that setup.py was not installing the bridge module.

--- setup.py
+++ setup.py
@@ -1,9 +1,34 @@
+"""cclib -- parsers and algorithms for computational chemistry
+
+cclib is a Python library that provides parsers for computational chemistry log files. It also provides a platform to implement algorithms in a package-independent manner.
+"""
+
 from distutils.core import setup
+
+# Chosen from http://www.python.org/pypi?:action=list_classifiers
+classifiers = """\
+Development Status :: 4 - Beta
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved :: GNU General Public License (GPL)
+Programming Language :: Python
+Topic :: Software Development :: Libraries :: Python Modules
+Operating System :: OS Independent
+Topic :: Scientific/Engineering :: Chemistry
+"""
+
+doclines = __doc__.split("\n")
+
 setup(name="cclib",
-      version="0.1",
-      author="Noel O'Boyle",
-      author_email="baoilleach@users.sf.net",
-      url="http://cclib.sourceforge.net",
+      version="0.5b",
+      author="Noel O'Boyle, Adam Tenderholt",
+      author_email="cclib-users@lists.sf.net",
+      url="http://cclib.sf.net",
+      description=doclines[0],
+      long_description = "\n".join(doclines[2:]),      
+      classifiers=filter(None, classifiers.split("\n")),
+      license="GPL",
+      platforms=["Any."],
       scripts=["src/scripts/ccget"],
       package_dir = {'cclib':'src/cclib'},
-      packages=['cclib','cclib.parser','cclib.progress','cclib.method'])
+      packages=['cclib','cclib.parser','cclib.progress','cclib.method','cclib.bridge'])
