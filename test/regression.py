bf57430b9be8151fad4b49f6180bfa8765c8782d
Noel O'Boyle <baoilleach@gmail.com>
2006-06-27 11:55:32 +0000

    Test: Added regression test for Mo5Obdt2-opt-c2v.adfout. This broke r227, but was fixed by r228 and r229.

--- test/regression.py
+++ test/regression.py
@@ -4,7 +4,8 @@ from cclib.parser import guesstype
 # The following files broke the parser in the past
 _brokeparser = ['Gaussian/wildGaussian98/oo-LAN.out',
                 'GAMESS/wildGAMESS-US/bv6010sup1molecule_4.inp.cml_.log',
-                'GAMESS/wildPCGAMESS/gamess.log' ]
+                'GAMESS/wildPCGAMESS/gamess.log',
+                'ADF/ADF2004.01/Mo5Obdt2-opt-c2v.adfout']
 for filename in _brokeparser:
     print "Attempting to parse %s." % filename
     newname = os.path.join("..","data",*filename.split("/")) # OS independent filenames
