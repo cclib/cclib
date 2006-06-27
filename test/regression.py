a460e9acc9919d0707a3bc1b9cd3c11dbf7c09ce
Noel O'Boyle <baoilleach@gmail.com>
2006-06-27 12:40:46 +0000

    Tests and data files: Removed all 'wild' data files (i.e. those that were not used for initial development of the parsers) as these will instead be in the test file repository, and have been sent to the googlemail account. Edited the paths to the test files in regression.py.

--- test/regression.py
+++ test/regression.py
@@ -2,9 +2,9 @@ import os
 from cclib.parser import guesstype
 
 # The following files broke the parser in the past
-_brokeparser = ['Gaussian/wildGaussian98/oo-LAN.out',
-                'GAMESS/wildGAMESS-US/bv6010sup1molecule_4.inp.cml_.log',
-                'GAMESS/wildPCGAMESS/gamess.log',
+_brokeparser = ['Gaussian/Gaussian98/oo-LAN.out',
+                'GAMESS/GAMESS-US/bv6010sup1molecule_4.inp.cml_.log',
+                'GAMESS/PCGAMESS/aomixgamess.log',
                 'ADF/ADF2004.01/Mo5Obdt2-opt-c2v.adfout']
 for filename in _brokeparser:
     print "Attempting to parse %s." % filename
