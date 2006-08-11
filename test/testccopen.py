63d560ea7cab165bc8caa332b17db8b36c02aa7a
Noel O'Boyle <baoilleach@gmail.com>
2006-08-11 16:03:51 +0000

    Tests: Try ccopening and parsing every data file we have...one possible problem with an ADF file.

--- test/testccopen.py
+++ test/testccopen.py
@@ -1,9 +1,52 @@
 import os
+import logging
+
+from glob import glob
+
 from cclib.parser import ccopen
+from cclib.parser import Gaussian, GAMESS, GAMESSUK, Jaguar, ADF
 
-os.chdir(os.path.join("..","data","Gaussian"))
+data = os.path.join("..","data")
+names = [ "Gaussian", "GAMESS", "ADF", "GAMESS UK", "Jaguar" ]
+dummyfiles = [ Gaussian(""), GAMESS(""), ADF(""), GAMESSUK(""), Jaguar("") ]
+filenames = [glob(os.path.join(data, "Gaussian", "basicGaussian03", "*.out")) + 
+             glob(os.path.join(data, "Gaussian", "basicGaussian03", "*.log")) +
+             glob(os.path.join(data, "Gaussian", "Gaussian03", "*.log")) +
+             glob(os.path.join(data, "Gaussian", "Gaussian98", "*.out")),
+             glob(os.path.join(data, "GAMESS", "basicGAMESS-US", "*.out")) +
+             glob(os.path.join(data, "GAMESS", "basicPCGAMESS", "*.out")) +
+             glob(os.path.join(data, "GAMESS", "GAMESS-US", "*.log")) +
+             glob(os.path.join(data, "GAMESS", "PCGAMESS", "*.log")),
+             glob(os.path.join(data, "ADF", "basicADF2004.01", "*.adfout")) +
+             glob(os.path.join(data, "ADF", "ADF2004.01", "*.adfout")),
+             glob(os.path.join(data, "GAMESS-UK", "basicGAMESS-UK", "*.out")),
+             glob(os.path.join(data, "Jaguar", "basicJaguar4.2", "*.out")) +
+             glob(os.path.join(data, "Jaguar", "basicJaguar6.5", "*.out")),
+             ]
 
-os.chdir("basicGaussian03")
+print
+failures = errors = total = 0
+for i in range(len(names)):
+    print "Are the %s files ccopened and parsed correctly?" % names[i]
+    for filename in filenames[i]:
+        total += 1
+        print "  %s..."  % filename,
+        try:
+            a  = ccopen(filename)
+        except:
+            errors += 1
+        else:
+            if type(a) == type(dummyfiles[i]):
+                try:
+                    a.logger.setLevel(logging.ERROR)
+                    a.parse()
+                    print "yes"
+                except:
+                    print "no"
+                    errors += 1
+            else:
+                print "file type not guessed correctly"
+                failures += 1
+    print
 
-for filename in ["dvb_gopt.out"]:
-    print ccopen(filename)
+print "Total: %d   Failed: %d  Errors: %d" % (total, failures, errors)
