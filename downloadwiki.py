9f4a4620555a53a18261cd2a9e5ed1b315bde16f
Noel O'Boyle <baoilleach@gmail.com>
2006-08-29 11:00:22 +0000

    Importing a script to download the text from the wiki, and also importing the SVG source of the logo (use Inkscape to view)

---
+++ downloadwiki.py
@@ -0,0 +1,21 @@
+import urllib
+from BeautifulSoup import BeautifulSoup
+
+wiki = "http://cclib.sourceforge.net/wiki"
+# http://openbabel.sourceforge.net/w/index.php?title=Special:Export&action=submit&pages=FAQ
+allpages = urllib.urlopen(wiki + "/index.php/Special:Allpages")    
+
+soup = BeautifulSoup(allpages)
+
+hrefs = []
+for anchor in soup('table')[2]('a'):
+    hrefs.append(anchor['href'].split("/")[-1])
+print hrefs
+
+params = urllib.urlencode({"action":"submit",
+                           "pages":"\n".join(hrefs)})
+
+query = urllib.urlopen(wiki + "/index.php/Special:Export", params)
+outputfile = open("backup.xml","w")
+print >> outputfile, query.read()
+outputfile.close()
