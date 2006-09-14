c587697430b438e115e2e7b0cf7b319d27e292f4
Noel O'Boyle <baoilleach@gmail.com>
2006-09-14 19:29:41 +0000

    Made change to manifest.py and hence to MANIFEST to include the unit tests and basic datafiles in the distribution.

--- manifest.py
+++ manifest.py
@@ -14,6 +14,17 @@ folders = ['method','bridge','parser','progress']
 for folder in folders:
     files.extend(glob.glob(os.path.join(source,folder,'*.py')))
 
+# Include data files
+folders = glob.glob(os.path.join('data', '*'))
+for folder in folders:
+    basicfolders = glob.glob(os.path.join(folder, 'basic*'))
+    for basicfolder in basicfolders:
+        files.extend(glob.glob(os.path.join(basicfolder, "*")))
+
+# Include test scripts
+files.extend(glob.glob(os.path.join("test", "test*.py")))
+files.append(os.path.join("test", "bettertest.py"))
+
 for file in files:
     if not os.path.isfile(file):
         print "%s does not exist" % file
