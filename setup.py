9202bdb0c3e56ca9f2c6b91b28df9f95d5cff993
Karol Langner <karol.langner@gmail.com>
2007-06-05 13:37:17 +0000

    Function setup in setup.py is now imported from setuptools if installed (see 'setup.py --help-commands' for new options). Made the file executable, added some classifiers and setup_cclib() function for clarity.

--- setup.py
+++ setup.py
@@ -1,36 +1,51 @@
-"""parsers and algorithms for computational chemistry
+#!/usr/bin/env python
+"""cclib: parsers and algorithms for computational chemistry
 
 cclib is a Python library that provides parsers for computational
 chemistry log files. It also provides a platform to implement
 algorithms in a package-independent manner.
 """
 
-from distutils.core import setup
+doclines = __doc__.split("\n")
 
 # Chosen from http://www.python.org/pypi?:action=list_classifiers
 classifiers = """\
 Development Status :: 4 - Beta
+Environment :: Console
 Intended Audience :: Science/Research
 Intended Audience :: Developers
 License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)
-Programming Language :: Python
-Topic :: Software Development :: Libraries :: Python Modules
+Natural Language :: English
 Operating System :: OS Independent
+Programming Language :: Python
 Topic :: Scientific/Engineering :: Chemistry
+Topic :: Software Development :: Libraries :: Python Modules
 """
 
-doclines = __doc__.split("\n")
+def setup_cclib():
 
-setup(name="cclib",
-      version="0.7",
-      author="cclib development team",
-      author_email="cclib-users@lists.sourceforge.net",
-      url="http://cclib.sf.net",
-      description=doclines[0],
-      long_description = "\n".join(doclines[2:]),      
-      classifiers=filter(None, classifiers.split("\n")),
-      license="LGPL",
-      platforms=["Any."],
-      scripts=["src/scripts/ccget", "src/scripts/cda"],
-      package_dir = {'cclib':'src/cclib'},
-      packages=['cclib','cclib.parser','cclib.progress','cclib.method','cclib.bridge'])
+    # If setuptools is installed, use the more advanced setup function.
+    try:
+        from setuptools import setup
+    except ImportError:
+        from distutils.core import setup
+
+    setup(
+        name = "cclib",
+        version = "0.7",
+        url = "http://cclib.sf.net",
+        author = "cclib development team",
+        author_email = "cclib-users@lists.sourceforge.net",
+        maintainer = "cclib development team",
+        maintainer_email = "cclib development team",
+        license = "LGPL",
+        description = doclines[0],
+        long_description = "\n".join(doclines[2:]),      
+        classifiers = filter(None, classifiers.split("\n")),
+        platforms = ["Any."],
+        scripts = ["src/scripts/ccget", "src/scripts/cda"],
+        package_dir = {'cclib':'src/cclib'},
+        packages = ['cclib', 'cclib.parser', 'cclib.progress', 'cclib.method', 'cclib.bridge'] )
+
+if __name__ == '__main__':
+    setup_cclib()
