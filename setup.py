c66e57af8a96ad1897c523d8d7146355d9e589fc
Noel O'Boyle <baoilleach@gmail.com>
2008-06-15 12:53:52 +0000

    Getting ready for release branch of cclib 0.9

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def setup_cclib():
 
     setup(
         name = "cclib",
-        version = "0.8",
+        version = "0.9b",
         url = "http://cclib.sf.net",
         author = "cclib development team",
         author_email = "cclib-users@lists.sourceforge.net",
