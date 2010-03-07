7f6da5616c17e2c50368df5c655ba546fb3b198d
Noel O'Boyle <baoilleach@gmail.com>
2010-03-07 18:42:22 +0000

    Update for cclib 1.0 release

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def setup_cclib():
 
     setup(
         name = "cclib",
-        version = "1.0b",
+        version = "1.0",
         url = "http://cclib.sf.net",
         author = "cclib development team",
         author_email = "cclib-users@lists.sourceforge.net",
--- src/cclib/__init__.py
+++ src/cclib/__init__.py
@@ -4,7 +4,7 @@ and licensed under the LGPL (http://www.gnu.org/copyleft/lgpl.html).
 """
 
 __revision__ = "$Revision$"
-__version__ = "1.0b"
+__version__ = "1.0"
 
 import parser
 import progress
