0eeba431f7d56ef23b7d3385a074740666dad2e9
Karol Langner <karol.langner@gmail.com>
2012-04-19 14:16:05 +0000

    Add more explicit license in source files, and allow LGPL version > 2.1

--- test/regression.py
+++ test/regression.py
@@ -1,3 +1,13 @@
+# This file is part of cclib (http://cclib.sf.net), a library for parsing
+# and interpreting the results of computational chemistry packages.
+#
+# Copyright (C) 2006, the cclib development team
+#
+# The library is free software, distributed under the terms of
+# the GNU Lesser General Public version 2.1 or later. You should have
+# received a copy of the license along with cclib. You can also access
+# the full license online at http://www.gnu.org/copyleft/lgpl.html.
+
 """
 A combined test framework for regression, ccopen and parsing which is
 designed to make it easy to add new tests or datafiles.
