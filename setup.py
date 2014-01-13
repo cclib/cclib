f2f6da814b72b486521cb7cd8082f3d1b208af06
Noel O'Boyle <baoilleach@gmail.com>
2014-01-13 19:16:50 +0000

    Remove references to wget.sh.

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ def setup_cclib():
     data_prefix = cclib_prefix + '/data'
     cclib_datafiles = [ (cclib_prefix, ['ANNOUNCE', 'CHANGELOG', 'INSTALL', 'LICENSE', 'README', 'THANKS']),
                         (test_prefix, ['test/testdata']),
-                        (data_prefix, ['data/regressionfiles.txt', 'data/wget.sh'])]
+                        (data_prefix, ['data/regressionfiles.txt', 'data/regression_download.sh'])]
     for program in programs:
         data_dirs = os.listdir('data/%s' %program)
         for data_dir in data_dirs:
