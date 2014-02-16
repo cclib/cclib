d87eba7a4d83c7ac0bfe664d60e7410d2e1c10eb
Karol M. Langner <karol.langner@gmail.com>
2014-02-15 23:30:04 -0500

    [regressions] Stash any uncommited changes before pulling regressions

--- data/regression_download.sh
+++ data/regression_download.sh
@@ -4,6 +4,10 @@ if [ -d regression/.git ]; then
     if [ -e regression/README.md ] && [ `head -1 regression/README.md` == "cclib-data" ]; then
         echo "Updating regression files..."
         cd regression
+        if [[ -n $(git status -s) ]]; then
+            echo "WARNING: stashing uncommited changes."
+            git stash save --keep-index
+        fi
         git pull
         cd ..
     else
