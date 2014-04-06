eca37e2049b73be28a477a8132e14d4d99f12330
Adam Tenderholt <atenderholt@gmail.com>
2014-04-06 08:44:11 -0700

    Add optdone attribute to ADF parser

--- src/cclib/parser/adfparser.py
+++ src/cclib/parser/adfparser.py
@@ -334,6 +334,7 @@ class ADF(logfileparser.Logfile):
             self.atomcoords.append(atomcoords)
             if self.finalgeometry == self.GETLAST: # Don't get any more coordinates
                 self.finalgeometry = self.NOMORE
+                self.optdone = True
 
         # There have been some changes in the format of the geometry convergence information,
         # and this is how it is printed in older versions (2007.01 unit tests).
