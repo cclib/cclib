6242faba6487a6245038a6ab75ab22db424db15a
Karol M. Langner <karol.langner@gmail.com>
2014-04-17 09:59:33 -0400

    Psi: add missing PsiBasisTest

--- test/testBasis.py
+++ test/testBasis.py
@@ -122,6 +122,10 @@ class PCGamessBasisTest(GenericBasisTest):
 class PCGamessBigBasisTest(GenericBigBasisTest):
     """PC-GAMESS big basis set unittest."""
 
+
+class PsiBasisTest(GenericBasisTest):
+    """Psi basis set unittest."""
+
               
 if __name__=="__main__":
 
