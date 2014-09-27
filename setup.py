735bc6175efb9569bbe7b576c4e19c950916889c
Karol M. Langner <karol.langner@gmail.com>
2014-09-27 17:07:58 -0400

    Some updates for 1.3b

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def setup_cclib():
 
     setup(
         name = "cclib",
-        version = "1.2",
+        version = "1.3b",
         url = "http://cclib.github.io/",
         author = "cclib development team",
         author_email = "cclib-users@lists.sourceforge.net",
