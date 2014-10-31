866b7f86b84c4cf16ec0ec30b3e18354e0f309f6
Karol M. Langner <karol.langner@gmail.com>
2014-10-30 23:07:57 -0400

    Fix typo

--- test/testBasis.py
+++ test/testBasis.py
@@ -8,7 +8,7 @@
 # received a copy of the license along with cclib. You can also access
 # the full license online at http://www.gnu.org/copyleft/lgpl.html.
 
-"""Test logfiles related to basis sets"
+"""Test logfiles related to basis sets"""
 
 import bettertest
 
