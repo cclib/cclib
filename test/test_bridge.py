cabda5dc0a3e921a5b2dc512b269f30dd2973d2f
Karol M. Langner <karol.langner@gmail.com>
2015-03-18 15:26:41 -0400

    Tests: add script for running bridge tests

---
+++ test/test_bridge.py
@@ -0,0 +1,24 @@
+# This file is part of cclib (http://cclib.github.io), a library for parsing
+# and interpreting the results of computational chemistry packages.
+#
+# Copyright (C) 2015, the cclib development team
+#
+# The library is free software, distributed under the terms of
+# the GNU Lesser General Public version 2.1 or later. You should have
+# received a copy of the license along with cclib. You can also access
+# the full license online at http://www.gnu.org/copyleft/lgpl.html.
+
+"""Run bridge unit tests for cclib."""
+
+from __future__ import print_function
+
+import sys
+
+import unittest
+
+sys.path.append("bridge")
+from testopenbabel import *
+
+
+if __name__ == "__main__":
+    unittest.main()
