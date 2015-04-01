f2b3a6f341acd0090e5e3c159afd5c65896d91b5
Karol M. Langner <karol.langner@gmail.com>
2015-04-01 14:29:49 -0400

    Tests: add some unit tests for ccData class

--- test/test_parser.py
+++ test/test_parser.py
@@ -20,6 +20,113 @@ import numpy
 import cclib
 
 
+class ccIOTest(unittest.TestCase):
+    """Unit tests for the ccio module."""
+
+    def test_guess_filetype_fail(self):
+        """Does the function fail as expected?"""
+        guess = cclib.parser.ccio.guess_filetype
+        self.assertIsNone(guess([]))
+        self.assertIsNone(guess(None))
+        self.assertIsNone(guess(os.devnull))
+        self.assertIsNone(guess(['test', 'random', 'quantum chemistry']))
+
+    def test_guess_filetype_program(self):
+        """Does the function catch all expected programs?"""
+        guess = cclib.parser.ccio.guess_filetype
+        self.assertEqual(guess(["Amsterdam Density Functional"]), cclib.parser.ADF)
+        self.assertEqual(guess(['Dalton - An Electronic Structure Program']), cclib.parser.DALTON)
+        self.assertEqual(guess(['GAMESS']), cclib.parser.GAMESS)
+        self.assertEqual(guess(['G A M E S S - U K']), cclib.parser.GAMESSUK)
+        self.assertEqual(guess(['Gaussian, Inc.']), cclib.parser.Gaussian)
+        self.assertEqual(guess(['Jaguar']), cclib.parser.Jaguar)
+        self.assertEqual(guess(['PROGRAM SYSTEM MOLPRO']), cclib.parser.Molpro)
+        self.assertEqual(guess(['Northwest Computational Chemistry Package']), cclib.parser.NWChem)
+        self.assertEqual(guess(['O   R   C   A']), cclib.parser.ORCA)
+        self.assertEqual(guess(["PSI ...Ab Initio Electronic Structure"]), cclib.parser.Psi)
+        self.assertEqual(guess(['A Quantum Leap Into The Future Of Chemistry']), cclib.parser.QChem)
+
+    def test_ccread_fail(self):
+        """Does the function fail as expected?"""
+        ccread = cclib.parser.ccio.ccread
+        self.assertIsNone(ccread("", quiet=True))
+        self.assertIsNone(ccread([], quiet=True))
+        self.assertIsNone(ccread(None, quiet=True))
+
+    def test_ccopen_fail(self):
+        """Does the function fail as expected?"""
+        ccopen = cclib.parser.ccio.ccread
+        self.assertIsNone(ccopen("", quiet=True))
+        self.assertIsNone(ccopen([], quiet=True))
+        self.assertIsNone(ccopen(None, quiet=True))
+
+    def test_fallback_fail(self):
+        """Does the functin fail as expected?"""
+        fallback = cclib.parser.ccio.fallback
+        self.assertIsNone(fallback(None))
+
+
+class DataTest(unittest.TestCase):
+    """Unit tests for the data module."""
+
+    check_array = ['atomcoords', 'scfenergies']
+    check_arrlist = ['mocoeffs', 'scfvalues']
+    check_arrdict = ['atomcharges', 'atomspins']
+
+    def setUp(self):
+        self.data = cclib.parser.logfileparser.ccData()
+
+    def _set_attributes(self, names, val):
+        for n in names:
+            setattr(self.data, n, val)
+
+    def test_listify_ndarray(self):
+        """Does the method convert ndarrays as expected?"""
+        self._set_attributes(self.check_array, numpy.array([1,2,3]))
+        self.data.listify()
+        for attr in self.check_array:
+            self.assertIsInstance(getattr(self.data, attr), list)
+
+    def test_listify_arraylist(self):
+        """Does the method convert lists of arrays as expected?"""
+        self._set_attributes(self.check_arrlist, [numpy.array([1,2,3]), numpy.array([4,5,6])])
+        self.data.listify()
+        for attr in self.check_arrlist:
+            for a in getattr(self.data, attr):
+                self.assertIsInstance(a, list)
+
+    def test_listify_arraydict(self):
+        """Does the method convert dicts of arrays as expected?"""
+        self._set_attributes(self.check_arrdict, {1: numpy.array([1,2,3]), 2: numpy.array([4,5,6])})
+        self.data.listify()
+        for attr in self.check_arrdict:
+            for a in getattr(self.data, attr).values():
+                self.assertIsInstance(a, list)
+
+    def test_arrayify_ndarray(self):
+        """Does the method convert lists as expected?"""
+        self._set_attributes(self.check_array, [1,2,3])
+        self.data.arrayify()
+        for attr in self.check_array:
+            self.assertIsInstance(getattr(self.data, attr), numpy.ndarray)
+
+    def test_arrayify_arraylist(self):
+        """Does the method convert lists of lists as expected?"""
+        self._set_attributes(self.check_arrdict, [[1,2,3], [4,5,6]])
+        self.data.arrayify()
+        for attr in self.check_arrdict:
+            for a in getattr(self.data, attr):
+                self.assertIsInstance(a, numpy.ndarray)
+
+    def test_arrayify_arraydict(self):
+        """Does the method convert dicts of lists as expected?"""
+        self._set_attributes(self.check_arrdict, {1: [1,2,3], 2: [4,5,6]})
+        self.data.arrayify()
+        for attr in self.check_arrdict:
+            for a in getattr(self.data, attr).values():
+                self.assertIsInstance(a, numpy.ndarray)
+
+
 class LogfileTest(unittest.TestCase):
     """Unit tests for the logfile module."""
 
@@ -46,52 +153,6 @@ class LogfileTest(unittest.TestCase):
         """Does this method return ERROR in base class?"""
         normalisesym = cclib.parser.logfileparser.Logfile('').normalisesym
         self.assertTrue("ERROR" in normalisesym(""))
-
-
-class ccIOTest(unittest.TestCase):
-    """Unit tests for the ccio module."""
-
-    def test_guess_filetype_fail(self):
-        """Does the function fail as expected?"""
-        guess = cclib.parser.ccio.guess_filetype
-        self.assertEqual(guess([]), None)
-        self.assertEqual(guess(None), None)
-        self.assertEqual(guess(os.devnull), None)
-
-    def test_guess_filetype_program(self):
-        """Does the function catch all expected programs?"""
-        guess = cclib.parser.ccio.guess_filetype
-        self.assertEqual(guess(['test', 'random', 'quantum chemistry']), None)
-        self.assertEqual(guess(["Amsterdam Density Functional"]), cclib.parser.ADF)
-        self.assertEqual(guess(['Dalton - An Electronic Structure Program']), cclib.parser.DALTON)
-        self.assertEqual(guess(['GAMESS']), cclib.parser.GAMESS)
-        self.assertEqual(guess(['G A M E S S - U K']), cclib.parser.GAMESSUK)
-        self.assertEqual(guess(['Gaussian, Inc.']), cclib.parser.Gaussian)
-        self.assertEqual(guess(['Jaguar']), cclib.parser.Jaguar)
-        self.assertEqual(guess(['PROGRAM SYSTEM MOLPRO']), cclib.parser.Molpro)
-        self.assertEqual(guess(['Northwest Computational Chemistry Package']), cclib.parser.NWChem)
-        self.assertEqual(guess(['O   R   C   A']), cclib.parser.ORCA)
-        self.assertEqual(guess(["PSI ...Ab Initio Electronic Structure"]), cclib.parser.Psi)
-        self.assertEqual(guess(['A Quantum Leap Into The Future Of Chemistry']), cclib.parser.QChem)
-
-    def test_ccread_fail(self):
-        """Does the function fail as expected?"""
-        ccread = cclib.parser.ccio.ccread
-        self.assertEquals(ccread("", quiet=True), None)
-        self.assertEquals(ccread([], quiet=True), None)
-        self.assertEquals(ccread(None, quiet=True), None)
-
-    def test_ccopen_fail(self):
-        """Does the function fail as expected?"""
-        ccopen = cclib.parser.ccio.ccread
-        self.assertEquals(ccopen("", quiet=True), None)
-        self.assertEquals(ccopen([], quiet=True), None)
-        self.assertEquals(ccopen(None, quiet=True), None)
-
-    def test_fallback_fail(self):
-        """Does the functin fail as expected?"""
-        fallback = cclib.parser.ccio.fallback
-        self.assertEquals(fallback(None), None)
 
 
 class UtilsTest(unittest.TestCase):
