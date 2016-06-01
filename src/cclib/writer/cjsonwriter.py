149a75a38bbbe552f05cc29c587dbf513483cd87
Sanjeed Schamnad <schamnad@amazon.com>
2016-06-01 22:32:43 +0530

    Generating the cjson using the Numpy encoder

--- src/cclib/writer/cjsonwriter.py
+++ src/cclib/writer/cjsonwriter.py
@@ -19,6 +19,8 @@ except ImportError:
 
 import os.path
 import json
+import base64
+import numpy as np
 
 from . import filewriter
 
@@ -132,7 +134,7 @@ class CJSON(filewriter.Writer):
             cjson_dict['diagram'] = self.pbmol.write(format='svg')
             
                  
-        return json.dumps(cjson_dict)
+        return json.dumps(cjson_dict, cls=NumpyEncoder)
 
     
     def hasData(self, attrNames):
@@ -374,6 +376,10 @@ class CJSON(filewriter.Writer):
             
             if hasattr(self.ccdata, 'vibirs') or hasattr(self.ccdata, 'vibramans'):
                 cjson_dict['Vibrations']['Intensities'] = dict()
+                if hasattr(self.ccdata, 'vibirs'):
+                    cjson_dict['Vibrations']['Intensities']['IR'] = self.ccdata.vibirs
+                else:
+                    cjson_dict['Vibrations']['Intensities']['Raman'] = self.ccdata.vibramans
                 
             attrList = ['vibsyms', 'hessian', 'vibdisps']
             availableAttrList = self.getAttrList(attrList)
@@ -430,6 +436,38 @@ class CJSON(filewriter.Writer):
             for attribute in availableAttr:
                 cjson_dict['Fragments'][attribute] = getattr(self.ccdata, attribute)
     
+
+class NumpyEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        """If input object is an ndarray it will be converted into a dict 
+        holding dtype, shape and the data, base64 encoded.
+        """
+        if isinstance(obj, np.ndarray):
+            if obj.flags['C_CONTIGUOUS']:
+                obj_data = obj.data
+            else:
+                cont_obj = np.ascontiguousarray(obj)
+                assert(cont_obj.flags['C_CONTIGUOUS'])
+                obj_data = cont_obj.data
+            data_b64 = base64.b64encode(obj_data)
+            return dict(__ndarray__=data_b64,
+                        dtype=str(obj.dtype),
+                        shape=obj.shape)
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder(self, obj)
+
+
+    def json_numpy_obj_hook(dct):
+        """Decodes a previously encoded numpy ndarray with proper shape and dtype.
+    
+        :param dct: (dict) json encoded ndarray
+        :return: (ndarray) if input was an encoded ndarray
+        """
+        if isinstance(dct, dict) and '__ndarray__' in dct:
+            data = base64.b64decode(dct['__ndarray__'])
+            return np.frombuffer(data, dct['dtype']).reshape(dct['shape'])
+        return dct
                 
 if __name__ == "__main__":
     pass
