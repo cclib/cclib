9b52f66770b2c797bb9dd69d3f02d0eb33a8cd6e
Eric Berquist <eric.berquist@gmail.com>
2016-07-09 19:21:25 -0400

    DALTON: fix for non-standard basis inputs

--- src/cclib/parser/daltonparser.py
+++ src/cclib/parser/daltonparser.py
@@ -57,6 +57,10 @@ class DALTON(logfileparser.Logfile):
         # If there is no symmetry, assume this.
         self.symlabels = ['Ag']
 
+        # Is the basis set from a single library file? This is true
+        # when the first line is BASIS, false for INTGRL/ATOMBASIS.
+        self.basislibrary = True
+
     def parse_geometry(self, lines):
         """Parse DALTON geometry lines into an atomcoords array."""
 
@@ -79,6 +83,11 @@ class DALTON(logfileparser.Logfile):
 
     def extract(self, inputfile, line):
         """Extract information from the file object inputfile."""
+
+        # Is the basis set from a single library file, or is it
+        # manually specified? See before_parsing().
+        if line[:6] == 'INTGRL'or line[:9] == 'ATOMBASIS':
+            self.basislibrary = False
 
         # This section at the start of geometry optimization jobs gives us information
         # about optimization targets (geotargets) and possibly other things as well.
@@ -222,7 +231,13 @@ class DALTON(logfileparser.Logfile):
             assert "Total number of atoms:" in line
             self.set_attribute("natom", int(line.split()[-1]))
 
-            self.skip_lines(inputfile, ['b', 'basisname', 'b'])
+            # When using the INTGRL keyword and not pulling from the
+            # basis set library, the "Basis set used" line doesn't
+            # appear.
+            if not self.basislibrary:
+                self.skip_line(inputfile, 'b')
+            else:
+                self.skip_lines(inputfile, ['b', 'basisname', 'b'])
 
             line = next(inputfile)
             cols = line.split()
