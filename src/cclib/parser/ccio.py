51f781f038c5c7e48ec07f1941b42f2fd0dfc6a0
Schamnad <sanjeed063@gmail.com>
2016-07-15 12:26:59 +0530

    Add --json flag to ccget script

--- src/cclib/parser/ccio.py
+++ src/cclib/parser/ccio.py
@@ -17,9 +17,6 @@ from __future__ import print_function
 
 import io
 import os
-import sys
-
-from . import data
 
 from . import logfileparser
 
@@ -34,7 +31,7 @@ from .nwchemparser import NWChem
 from .orcaparser import ORCA
 from .psiparser import Psi
 from .qchemparser import QChem
-from .cjsonreader import CJSON
+from ..writer.cjsonreader import CJSON
 
 try:
     from ..bridge import cclib2openbabel
@@ -69,7 +66,6 @@ triggers = [
     (ORCA,      ["O   R   C   A"],                                  True),
     (Psi,       ["PSI", "Ab Initio Electronic Structure"],          True),
     (QChem,     ["A Quantum Leap Into The Future Of Chemistry"],    True),
-    (CJSON,     ["chemical json"],                                  True)
 
 ]
 
@@ -106,7 +102,12 @@ def ccread(source, *args, **kargs):
     if log:
         if kargs.get('verbose', None):
             print('Identified logfile to be in %s format' % log.logname)
-        return log.parse()
+        # If the input file is a CJSON file and not a standard compchemlog file
+        cjson_as_input = kargs.get("cjson", False)
+        if cjson_as_input:
+            return log.open_cjson()
+        else:
+            return log.parse()
     else:
         if kargs.get('verbose', None):
             print('Attempting to use fallback mechanism to read file')
@@ -164,7 +165,14 @@ def ccopen(source, *args, **kargs):
     # Proceed to return an instance of the logfile parser only if the filetype
     # could be guessed. Need to make sure the input file is closed before creating
     # an instance, because parsers will handle opening/closing on their own.
-    filetype = guess_filetype(inputfile)
+    # If the input file is a CJSON file and not a standard compchemlog file, don't
+    # guess the file
+    cjson_as_input = kargs.get("cjson", False)
+    if cjson_as_input:
+        filetype = CJSON
+    else:
+        filetype = guess_filetype(inputfile)
+
     if filetype:
         inputfile.seek(0, 0)
         if not isstream:
