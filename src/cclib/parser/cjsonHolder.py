e182d4d0b4675cbc448ea069250db8375e191769
Schamnad <sanjeed063@gmail.com>
2016-07-18 00:41:56 +0530

    Fix import statement for cjsonReader in ccio.py

--- src/cclib/parser/cjsonHolder.py
+++ src/cclib/parser/cjsonHolder.py
@@ -124,7 +124,6 @@ class cjsonHolder:
 
     def compress(self):
         self.cjson = self.purge_cjson(self.cjson)
-        #print(self.cjson)
 
     def __getitem__(self, key):
         return getattr(self, key)
