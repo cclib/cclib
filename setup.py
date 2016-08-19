e11f810b31574047db79f4cd48540515cfad8080
Karol M. Langner <karol.langner@gmail.com>
2016-08-19 09:36:19 -0700

    Bump v1.4.1 to v1.5

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ def setup_cclib():
 
     setup(
         name = "cclib",
-        version = "1.4.1",
+        version = "1.5",
         url = "http://cclib.github.io/",
         author = "cclib development team",
         author_email = "cclib-users@lists.sourceforge.net",
