7fcd124462dbeb87b018592edd69f91c9512c76a
Eric Berquist <eric.berquist@gmail.com>
2017-08-26 10:10:09 -0400

    BOMD: custom test for Gaussian energy printing count

--- test/data/testBOMD.py
+++ test/data/testBOMD.py
@@ -17,27 +17,37 @@ __filedir__ = os.path.realpath(os.path.dirname(__file__))
 class GenericBOMDTest(unittest.TestCase):
     """Generic Born-Oppenheimer molecular dynamics unittest"""
 
+    # To calculate the initial set of forces/velocities, programs
+    # first converge the energy at the input geometry, so there is
+    # always one more geometry/energy than MD step.
     nsteps = 35
-    natoms = 20
+    nenergies = 36
 
     def testdimscfenergies(self):
         """Are the number of parsed energies consistent with the number of MD
         steps?
         """
-        self.assertEquals(self.data.scfenergies.shape, (self.nsteps, ))
+        self.assertEquals(self.data.scfenergies.shape, (self.nenergies, ))
 
     def testdimatomcoords(self):
         """Are the number of parsed geometries consistent with the number of
         MD steps?
         """
-        self.assertEquals(self.data.atomcoords.shape, (self.nsteps, self.natoms, 3))
+        self.assertEquals(self.data.atomcoords.shape, (self.nenergies, 20, 3))
 
     def testdimtime(self):
         """Are the number of time points consistent with the number of MD
         steps?
         """
-        print(self.data.time)
         self.assertEquals(self.data.time.shape, (self.nsteps, ))
+
+
+class GaussianBOMDTest(GenericBOMDTest):
+    """Customized Born-Oppenheimer molecular dynamics unittest"""
+
+    # This may have something to do with our corrections for
+    # extrapolation step rejection.
+    nenergies = 35
 
 
 if __name__=="__main__":
