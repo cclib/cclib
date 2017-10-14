8ea9723cf201ad60d488509c2382f080a7e23f87
mcocdawc <oskar.weser@gmail.com>
2017-10-14 11:29:13 +0200

    Revert "MAINT: Changed setup.py script"
    This reverts commit 6c56dc8fbc8faee2ae5457c812de3d7471875f4b.

--- setup.py
+++ setup.py
@@ -12,10 +12,7 @@ chemistry log files. It also provides a platform to implement
 algorithms in a package-independent manner.
 """
 
-from __future__ import with_statement
-from __future__ import absolute_import
-from setuptools import setup, find_packages
-from io import open  # pylint:disable=redefined-builtin
+import sys
 
 
 # Chosen from http://www.python.org/pypi?:action=list_classifiers
@@ -31,34 +28,46 @@ Topic :: Scientific/Engineering :: Chemistry
 Topic :: Software Development :: Libraries :: Python Modules"""
 
 
-def readme():
-    '''Return the contents of the README.md file.'''
-    with open('README.md') as freadme:
-        return freadme.read()
-
-
 def setup_cclib():
+
+    # Import from setuptools only if requested.
+    if 'egg' in sys.argv:
+        sys.argv.pop(sys.argv.index('egg'))
+        from setuptools import setup
+
+    from distutils.core import setup
+
+    # The list of packages to be installed.
+    cclib_packages = [
+        'cclib',
+        'cclib.bridge',
+        'cclib.io',
+        'cclib.method',
+        'cclib.parser',
+        'cclib.progress',
+    ]
 
     doclines = __doc__.split("\n")
 
     setup(
-        name="cclib",
-        version="1.5.2",
-        url="http://cclib.github.io/",
-        author="cclib development team",
-        author_email="cclib-users@lists.sourceforge.net",
-        maintainer="cclib development team",
-        maintainer_email="cclib-users@lists.sourceforge.net",
-        license="BSD 3-Clause License",
-        description=doclines[0],
-        long_description="\n".join(doclines[2:]),
-        classifiers=classifiers.split("\n"),
-        platforms=["Any."],
-        packages=find_packages('src'),
-        package_dir={'': 'src'},
-        scripts=["src/scripts/ccget", "src/scripts/ccwrite", "src/scripts/cda"]
+        name = "cclib",
+        version = "1.5.2",
+        url = "http://cclib.github.io/",
+        author = "cclib development team",
+        author_email = "cclib-users@lists.sourceforge.net",
+        maintainer = "cclib development team",
+        maintainer_email = "cclib-users@lists.sourceforge.net",
+        license = "BSD 3-Clause License",
+        description = doclines[0],
+        long_description = "\n".join(doclines[2:]),
+        classifiers = classifiers.split("\n"),
+        platforms = ["Any."],
+        packages = cclib_packages,
+        package_dir = { 'cclib':'src/cclib' },
+        scripts = ["src/scripts/ccget", "src/scripts/ccwrite", "src/scripts/cda"],
     )
 
 
 if __name__ == '__main__':
+
     setup_cclib()
