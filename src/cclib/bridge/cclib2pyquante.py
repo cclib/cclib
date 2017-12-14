1bae9cb8c345b3fe61dd6e7b91b38b75fb1f297e
Eric Berquist <eric.berquist@gmail.com>
2017-12-14 12:24:40 -0500

    fix energy in PyQuante doctest

--- src/cclib/bridge/cclib2pyquante.py
+++ src/cclib/bridge/cclib2pyquante.py
@@ -27,8 +27,8 @@ def makepyquante(atomcoords, atomnos, charge=0, mult=1):
     >>> a = numpy.array([[-1,1,0],[0,0,0],[1,1,0]],"f")
     >>> pyqmol = makepyquante(a,atomnos)
     >>> en, orbe, orbs = hf(pyqmol)
-    >>> print(en) # Should be around -73.8
-    -73.8
+    >>> en
+    -75.8247540498
     """
     return Molecule("notitle", list(zip(atomnos, atomcoords)), units="Angstrom",
                     charge=charge, multiplicity=mult)
