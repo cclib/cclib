7b8b2bf2b086ac594e673fcb102cdb5ceaf4194c
Eric Berquist <eric.berquist@gmail.com>
2017-12-14 12:08:49 -0500

    Chain tests together so bash stops on first failure and returns non-zero

--- travis/run_pytest.sh
+++ travis/run_pytest.sh
@@ -3,8 +3,6 @@
 # run_pytest.sh: Run pytest on cclib with coverage checking. Requires `pytest`
 # and `pytest-cov`.
 
-pytest -v --doctest-modules --capture=no --cov=cclib src test
-cd data
-bash ./regression_download.sh
-cd ..
+pytest -v --doctest-modules --capture=no --cov=cclib src test &&
+cd data && bash ./regression_download.sh && cd .. &&
 pytest -v --capture=no --cov=cclib --cov-append -k test_regression test/regression.py
