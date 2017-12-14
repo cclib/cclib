3ff22a20175b85a487652eea686fd81fc03b2a5a
Eric Berquist <eric.berquist@gmail.com>
2017-12-14 13:43:27 -0500

    call pytest as a module

--- travis/run_pytest.sh
+++ travis/run_pytest.sh
@@ -3,6 +3,6 @@
 # run_pytest.sh: Run pytest on cclib with coverage checking. Requires `pytest`
 # and `pytest-cov`.
 
-pytest -v --doctest-modules --capture=no --cov=cclib src test &&
+python -m pytest -v --doctest-modules --capture=no --cov=cclib src test &&
 cd data && bash ./regression_download.sh && cd .. &&
-pytest -v --capture=no --cov=cclib --cov-append -k test_regression test/regression.py
+python -m pytest -v --capture=no --cov=cclib --cov-append -k test_regression test/regression.py
