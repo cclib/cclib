c844d9f09183ab616f72131351aed9c77aa830d0
Eric Berquist <eric.berquist@gmail.com>
2018-04-06 11:13:39 -0400

    descriptive names for ignoring paths in pytest

--- test/conftest.py
+++ test/conftest.py
@@ -5,17 +5,17 @@ version_major = sys.version_info.major
 version_minor = sys.version_info.minor
 
 # Paths that should be ignored for all Python versions.
-paths_allver = [
+paths_ignore_allver = [
     'src/cclib/progress/qt4progress.py',
 ]
 
 # Paths that should run only for Python 2.7.
-paths_only_2_7 = [
+paths_ignore_only_2_7 = [
     'src/cclib/bridge/cclib2pyquante.py',
 ]
 
 # Paths that should run everywhere except Python 3.2.
-paths_not_3_2 = [
+paths_ignore_not_3_2 = [
     'src/cclib/bridge/cclib2biopython.py',
 ]
 
@@ -28,12 +28,12 @@ def match_path(path, partial_paths):
 
 def pytest_ignore_collect(path, config):
     """If this returns True for a given path, pytest will ignore it."""
-    if match_path(path, paths_allver):
+    if match_path(path, paths_ignore_allver):
         return True
     if version_major != 2:
-        if match_path(path, paths_only_2_7):
+        if match_path(path, paths_ignore_only_2_7):
             return True
     if version_major == 3 and version_minor == 2:
-        if match_path(path, paths_not_3_2):
+        if match_path(path, paths_ignore_not_3_2):
             return True
     return False
