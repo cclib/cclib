9726d4f04302525c54cc876380a3a6c32a20fa74
Eric Berquist <eric.berquist@gmail.com>
2018-08-29 16:38:15 -0400

    docs: Replace travis-sphinx with standalone deploy script

--- travis/deploy_docs_travis-sphinx.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# deploy_docs_travis-sphinx.sh: Deploy the built documentation to
-# GitHub Pages using Travis CI and travis-sphinx.
-
-MESSAGE="Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER} Commit: ${TRAVIS_COMMIT}"
-travis-sphinx --verbose deploy --message="${MESSAGE}"
