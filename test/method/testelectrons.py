c8b0895a617bcfbdceffd930deff5ce46d788b17
Jaime RG <jaime.rogue@gmail.com>
2018-09-02 22:55:36 +0200

    add alpha and beta electrons tests

--- test/method/testelectrons.py
+++ test/method/testelectrons.py
@@ -43,6 +43,22 @@ class ElectronsTest(unittest.TestCase):
         self.assertEqual(Electrons(data).count(), 120)
         self.assertEqual(Electrons(data).count(core=True), 188)
 
+    def test_alpha_beta(self):
+        """Test number of alpha and beta electrons"""
+        # Systems with alpha == beta
+        data, logfile = getdatafile(QChem, "basicQChem4.2", ["water_mp4sdq.out"])
+        self.assertEqual(Electrons(data).alpha(), 5)
+        self.assertEqual(Electrons(data).beta(), 5)
+        data, logfile = getdatafile(Gaussian, "basicGaussian09", ["water_cis.log"])
+        self.assertEqual(Electrons(data).alpha(), 5)
+        self.assertEqual(Electrons(data).beta(), 5)
+        # Systems with alpha != beta
+        data, logfile = getdatafile(QChem, "basicQChem4.2", ["dvb_sp_un.out"])
+        self.assertEqual(Electrons(data).alpha(), 35)
+        self.assertEqual(Electrons(data).beta(), 34)
+        data, logfile = getdatafile(Gaussian, "basicGaussian09", ["dvb_un_sp.log"])
+        self.assertEqual(Electrons(data).alpha(), 35)
+        self.assertEqual(Electrons(data).beta(), 34)
 
 if __name__ == "__main__":
     unittest.TextTestRunner(verbosity=2).run(unittest.makeSuite(ElectronsTest))
