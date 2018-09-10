9b9436a1acdaa3dd531b2540689e27ff36b399ba
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2018-09-10 20:27:04 +0300

    Test with idealized inputs instead of parsed data

--- test/method/testmoments.py
+++ test/method/testmoments.py
@@ -11,59 +11,109 @@ from __future__ import print_function
 
 import sys
 import unittest
+from unittest import mock
 
-from numpy.testing import assert_almost_equal
+import numpy as np
+from numpy.testing import assert_equal, assert_almost_equal
 
 from cclib.method import Moments
-from cclib.parser import GAMESS, Gaussian
-
-sys.path.insert(1, "..")
-
-from ..test_data import getdatafile
 
 
-class MomentsTest(unittest.TestCase):
-    def test_results(self):
-        data, _ = getdatafile(Gaussian, "basicGaussian16", ["water_mp2.log"])
-        x = Moments(data).calculate()
-        assert_almost_equal(x[0], [0, 0, 0], 5)
-        assert_almost_equal(x[1], [0, 0, -0.91543], 5)
-        assert_almost_equal(x[2][0] + x[2][3] + x[2][5], 0)
+class TestIdealizedInputs(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.linear_dipole_attrs = {
+            'atomcoords': np.array([[[-1, 0, 0], [ 1,  0, 0]]]),
+            'atomcharges': {'mulliken': [-1, 1]},
+            'atomnos': [1, 1],
+            'charge': 0,
+        }
+    
+    @mock.patch('cclib.parser.ccData', spec=True)
+    def test_dipole_moment(self, mock):
+        mock.configure_mock(**self.linear_dipole_attrs)
 
-    def test_origin_displacement(self):
-        data, _ = getdatafile(Gaussian, "basicGaussian16", ["water_mp2.log"])
-        x = Moments(data).calculate()
-        y = Moments(data).calculate(origin=[2e7, 18, 28])
-        assert_almost_equal(x[1], y[1])
+        x = Moments(mock).calculate()[1]
+        assert_almost_equal(x / 4.803204252680386, [2, 0, 0])
 
-    def test_origin_at_center_of_nuclear_charge(self):
-        data, _ = getdatafile(Gaussian, "basicGaussian16", ["water_mp2.log"])
-        x = Moments(data).calculate(origin="nuccharge")
-        assert_almost_equal(x[0], [0, 0, 0], 6)
+    @mock.patch('cclib.parser.ccData', spec=True)
+    def test_quarupole_moment(self, mock):
+        mock.charge = 0
+        mock.atomcoords = np.array([[
+            [-0.75,  1, 0],
+            [ 1,  1, 0],
+            [ 0.75, -1, 0],
+            [-1, -1, 0]]])
+        mock.atomcharges = {'mulliken': [-1, 1, -1, 1]}
+        mock.atomnos = np.ones(mock.atomcoords.shape[1])
 
-    def test_origin_at_center_of_mass(self):
-        data, _ = getdatafile(Gaussian, "basicGaussian16", ["water_mp2.log"])
-        x = Moments(data).calculate(origin="mass")
-        assert_almost_equal(x[0], [0, 0, 0.0524806])
+        x = Moments(mock).calculate()[2]
+        assert np.isclose(x[0] + x[3] + x[5], 0)
+        assert np.isclose(x[0], -x[3] * 2)
+        assert np.isclose(x[0], -x[5] * 2)
 
-    def test_user_provided_origin(self):
-        data, _ = getdatafile(Gaussian, "basicGaussian16", ["water_mp2.log"])
-        x = Moments(data).calculate(origin=[1, 1, 1])
-        assert_almost_equal(x[0], [1, 1, 1])
+    @mock.patch('cclib.parser.ccData', spec=True)
+    def test_invariant_to_origin_dislacement(self, mock):
+        mock.configure_mock(**self.linear_dipole_attrs)
+
+        x = Moments(mock).calculate(origin=[0, 0, 0])[1]
+        y = Moments(mock).calculate(origin=[1, 1, 1])[1]
+        assert_equal(x, y)
+    
+    @mock.patch('cclib.parser.ccData', spec=True)
+    def test_variant_to_origin_dislacement(self, mock):
+        attrs = dict(self.linear_dipole_attrs, **{
+            'charge': 1,
+            'atomcharges': {'mulliken': [-1, 2]}
+        })
+        mock.configure_mock(**attrs)
+
+        x = Moments(mock).calculate(origin=[0, 0, 0])[1]
+        y = Moments(mock).calculate(origin=[1, 1, 1])[1]
+        assert not np.array_equal(x, y)
+
+    @mock.patch('cclib.parser.ccData', spec=True)
+    def test_origin_at_center_of_nuclear_charge(self, mock):
+        mock.configure_mock(**self.linear_dipole_attrs)
+
+        x = Moments(mock).calculate()[0]
+        assert_equal(x, [0, 0, 0])
         
-    def test_user_provided_masses(self):
-        data, _ = getdatafile(Gaussian, "basicGaussian16", ["water_mp2.log"])
-        x = Moments(data).calculate(masses=[1, 1, 1], origin="mass")
-        assert_almost_equal(x[0], [0, 0, -0.2780383])
+    @mock.patch('cclib.parser.ccData', spec=True)
+    def test_origin_at_center_of_mass(self, mock):
+        mock.configure_mock(**self.linear_dipole_attrs)
+        mock.atommasses = np.ones(mock.atomcoords.shape[1])
 
-    def test_results_storing(self):
-        data, _ = getdatafile(GAMESS, "basicFirefly8.0", ["water_mp2.out"])
-        m = Moments(data)
+        x = Moments(mock).calculate(origin='mass')[0]
+        assert_equal(x, [0, 0, 0])
+
+    @mock.patch('cclib.parser.ccData', spec=True)
+    def test_user_provided_origin(self, mock):
+        mock.configure_mock(**self.linear_dipole_attrs)
+
+        x = Moments(mock).calculate(origin=[1, 1, 1])
+        assert_almost_equal(x[0], [1, 1, 1])
+
+    @mock.patch('cclib.parser.ccData', spec=True)
+    def test_user_provided_masses(self, mock):
+        mock.configure_mock(**self.linear_dipole_attrs)
+
+        x = Moments(mock).calculate(masses=[1, 1], origin="mass")
+        assert_almost_equal(x[0], [0, 0, 0])
+
+    @mock.patch('cclib.parser.ccData', spec=True)
+    def test_results_storing(self, mock):
+        mock.configure_mock(**self.linear_dipole_attrs)
+        mock.atomcharges.update({'lowdin': [-0.5, 0.5]})
+        
+        m = Moments(mock)
         m.calculate(population='mulliken')
         m.calculate(population='lowdin')
-        assert 'mulliken' in m.results
-        assert 'lowdin' in m.results
+
+        a, b = m.results['mulliken'][1], m.results['lowdin'][1]
+        assert not np.array_equal(a, b)
 
         
 if __name__ == "__main__":
-    unittest.TextTestRunner(verbosity=2).run(unittest.makeSuite(MomentsTest))
+    suite = unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
+    unittest.TextTestRunner(verbosity=2).run(suite)
