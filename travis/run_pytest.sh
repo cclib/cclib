a99cfa5f4c9cf84761f5ae1b760ea74f221ca21b
Eric Berquist <eric.berquist@gmail.com>
2018-11-07 23:30:25 -0500

    Testing regressions should return an error code on failure, no matter what

--- travis/run_pytest.sh
+++ travis/run_pytest.sh
@@ -3,8 +3,10 @@
 # run_pytest.sh: Run pytest on cclib with coverage checking. Requires `pytest`
 # and `pytest-cov`.
 
-set -eu
+set -euxo pipefail
 
-python -m pytest -v --capture=no --cov=cclib --cov-report=term --cov-report=html test &&
-cd data && bash ./regression_download.sh && cd .. &&
+python -m pytest -v --capture=no --cov=cclib --cov-report=term --cov-report=html test
+pushd data
+bash ./regression_download.sh
+popd
 python -m pytest -v --capture=no --cov=cclib --cov-report=term --cov-report=html --cov-append -k test_regression test/regression.py
