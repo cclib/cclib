11479cf5c3398f4b8b3e2936226efe77afea2a4f
Eric Berquist <eric.berquist@gmail.com>
2018-12-20 22:42:59 -0500

    QChem: update from 4.2 -> 5.1 in visual test

--- test/test_data.py
+++ test/test_data.py
@@ -250,7 +250,7 @@ class DataSuite(object):
             'NWChem6.5' : getdatafile('NWChem', "basicNWChem6.5", ["dvb_gopt_ks.out"])[0],
             'ORCA4.0' : getdatafile('ORCA', "basicORCA4.0", ["dvb_gopt.out"])[0],
             'Psi4-1.0' : getdatafile('Psi4', "basicPsi4-1.0", ["dvb_gopt_rks.out"])[0],
-            'QChem4.2' : getdatafile('QChem', "basicQChem4.2", ["dvb_gopt.out"])[0],
+            'QChem5.1' : getdatafile('QChem', "basicQChem5.1", ["dvb_gopt.out"])[0],
         }
         parser_names = sorted(parsers_to_test.keys())
         output = [parsers_to_test[pn] for pn in parser_names]
