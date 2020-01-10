92e4e3af0cd0cfc30af2c1e0d8720bf7ce19169a
Alex Maldonado <alex.maldonado113@gmail.com>
2020-01-09 22:09:47 -0500

    Parsing ORCA MP2 gradients (#788)

--- cclib/parser/orcaparser.py
+++ cclib/parser/orcaparser.py
@@ -388,21 +388,30 @@ class ORCA(logfileparser.Logfile):
         # 1   H   :    0.000000004    0.019501450   -0.021537091
         # 2   O   :    0.000000054   -0.042431648    0.042431420
         # 3   H   :    0.000000004    0.021537179   -0.019501388
-        if line[:18] == 'CARTESIAN GRADIENT':
-            next(inputfile)
-            next(inputfile)
+        #
+        # ORCA MP2 module has different signal than 'CARTESIAN GRADIENT'.
+        #
+        # The final MP2 gradient
+        # 0:   0.01527469  -0.00292883   0.01125000
+        # 1:   0.00098782  -0.00040549   0.00196825
+        # 2:  -0.01626251   0.00333431  -0.01321825
+        if line[:18] == 'CARTESIAN GRADIENT' or line[:22] == 'The final MP2 gradient':
 
             grads = []
+            if line[:18] == 'CARTESIAN GRADIENT':
+                self.skip_lines(inputfile, ['dashes', 'blank'])
+
             line = next(inputfile).strip()
             while line:
-                idx, atom, colon, x, y, z = line.split()
-                grads.append((float(x), float(y), float(z)))
-
+                tokens = line.split()
+                x, y, z = float(tokens[-3]), float(tokens[-2]), float(tokens[-1])
+                grads.append((x, y, z))
                 line = next(inputfile).strip()
 
             if not hasattr(self, 'grads'):
                 self.grads = []
             self.grads.append(grads)
+
 
         # After each geometry optimization step, ORCA prints the current convergence
         # parameters and the targets (again), so it is a good idea to check that they
