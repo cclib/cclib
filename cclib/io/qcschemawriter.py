1a6631b988455092d75f65da1a4c99c20982b42c
Eric Berquist <eric.berquist@gmail.com>
2020-09-28 01:00:37 -0400

    QCSchema output writer: crash on unknown driver or method

--- cclib/io/qcschemawriter.py
+++ cclib/io/qcschemawriter.py
@@ -91,7 +91,7 @@ class QCSchemaWriter(CJSONWriter):
             )
             return_energy = ccsd_total_energy
         else:
-            pass
+            raise RuntimeError("Don't know what to do with method {}".format(method))
 
         qcschema_dict["properties"] = {
             "calcinfo_nbasis": self.ccdata.nbasis,
@@ -135,7 +135,11 @@ class QCSchemaWriter(CJSONWriter):
             )
 
         # TODO gradient, Hessian
-        qcschema_dict["return_result"] = return_energy
+        if driver == "energy":
+            return_result = return_energy
+        else:
+            raise RuntimeError("Don't know driver {}".format(driver))
+        qcschema_dict["return_result"] = return_result
 
         return qcschema_dict
 
