cde46785cee38e3871e6841183590dfca1426950
Eric Berquist <eric.berquist@gmail.com>
2020-09-28 00:45:00 -0400

    Add more QCSchema output fields

--- cclib/io/qcjsonwriter.py
+++ cclib/io/qcjsonwriter.py
@@ -28,12 +28,19 @@ class QCJSONWriter(CJSONWriter):
         qcschema_dict["schema_name"] = "qcschema_output"
         qcschema_dict["schema_version"] = 1
 
-        qcschema_dict["molecule"] = dict()
-        qcschema_dict["molecule"]["schema_name"] = "qcschema_molecule"
-        qcschema_dict["molecule"]["schema_version"] = 2
+        qcschema_dict["molecule"] = {
+            "schema_name": "qcschema_molecule",
+            "schema_version": 2,
+        }
 
-        # FIXME hack
-        qcschema_dict["driver"] = "energy"
+        # TODO make this an enum
+        if hasattr(self.ccdata, "hessian"):
+            driver = "Hessian"
+        elif hasattr(self.ccdata, "grads"):
+            driver = "gradient"
+        else:
+            driver = "energy"
+        qcschema_dict["driver"] = driver
 
         metadata = self.ccdata.metadata
 
@@ -45,33 +52,21 @@ class QCJSONWriter(CJSONWriter):
         # else:
         qcschema_dict["keywords"] = dict()
 
-        qcschema_dict["model"] = dict()
         # TODO methods should be an enum
         if metadata["methods"]:
             method = metadata["methods"][-1]
         else:
             # FIXME
             method = "HF"
-        qcschema_dict["model"]["method"] = method
 
-        # FIXME
-        qcschema_dict["model"]["basis"] = ""
+        qcschema_dict["model"] = {"method": method, "basis": metadata["basis_set"]}
 
-        qcschema_dict["provenance"] = dict()
-        qcschema_dict["provenance"]["creator"] = metadata["package"]
-        qcschema_dict["provenance"]["version"] = metadata["package_version"]
-        # FIXME
-        qcschema_dict["provenance"]["routine"] = ""
-
-        qcschema_dict["properties"] = dict()
-        qcschema_dict["properties"]["calcinfo_nbasis"] = self.ccdata.nbasis
-        qcschema_dict["properties"]["calcinfo_nmo"] = self.ccdata.nmo
-        # TODO nalpha and nbeta
-        qcschema_dict["properties"]["calcinfo_natom"] = self.ccdata.natom
-
-        qcschema_dict["properties"]["scf_total_energy"] = convertor(
-            self.ccdata.scfenergies[-1], "eV", "hartree"
-        )
+        qcschema_dict["provenance"] = {
+            "creator": metadata["package"],
+            "version": metadata["package_version"],
+            # FIXME
+            "routine": "",
+        }
 
         qcschema_dict["molecule"] = {
             "schema_name": "qcschema_molecule",
@@ -80,14 +75,76 @@ class QCJSONWriter(CJSONWriter):
             "symbols": [self.pt.element[atomno] for atomno in self.ccdata.atomnos],
         }
 
+        scf_total_energy = convertor(self.ccdata.scfenergies[-1], "eV", "hartree")
+        mp2_correlation_energy = None
+        mp2_total_energy = None
+        ccsd_correlation_energy = None
+        ccsd_total_energy = None
+
         if method == "HF":
-            return_result = self.ccdata.scfenergies[-1]
+            return_energy = scf_total_energy
         elif method == "CCSD":
-            # FIXME for CCSD(T) etc.
-            return_result = self.ccdata.ccenergies[-1]
+            mp2_total_energy = convertor(
+                self.ccdata.mpenergies[-1][-1], "eV", "hartree"
+            )
+            mp2_correlation_energy = convertor(
+                self.ccdata.mpenergies[-1][-1] - self.ccdata.scfenergies[-1],
+                "eV",
+                "hartree",
+            )
+            ccsd_total_energy = convertor(self.ccdata.ccenergies[-1], "eV", "hartree")
+            ccsd_correlation_energy = convertor(
+                self.ccdata.ccenergies[-1] - self.ccdata.scfenergies[-1],
+                "eV",
+                "hartree",
+            )
+            return_energy = ccsd_total_energy
         else:
             pass
-        qcschema_dict["return_result"] = return_result
+
+        qcschema_dict["properties"] = {
+            "calcinfo_nbasis": self.ccdata.nbasis,
+            "calcinfo_nmo": self.ccdata.nmo,
+            "calcinfo_natom": self.ccdata.natom,
+            "calcinfo_nalpha": self.ccdata.homos[0] + 1,
+            "calcinfo_nbeta": self.ccdata.homos[-1] + 1,
+            "scf_iterations": self.ccdata.scfvalues[-1].shape[0],
+            # TODO check units
+            "scf_dipole_moment": self.ccdata.moments[1].tolist(),
+            "scf_total_energy": scf_total_energy,
+            "return_energy": return_energy,
+            # TODO scf_one_electron energy
+            # TODO scf_two_electron_energy
+            # TODO nuclear_repulsion_energy
+            # TODO mp2_same_spin_correlation_energy
+            # TODO mp2_opposite_spin_correlation_energy
+            # TODO mp2_singles_energy
+            # TODO mp2_doubles_energy
+            # TODO ccsd_same_spin_correlation_energy
+            # TODO ccsd_opposite_spin_correlation_energy
+            # TODO ccsd_singles_energy
+            # TODO ccsd_doubles_energy
+            # TODO ccsd_prt_pr_correlation_energy
+            # TODO ccsd_prt_pr_total_energy
+            # TODO ccsd_iterations
+        }
+        if mp2_correlation_energy is not None:
+            qcschema_dict["properties"].update(
+                {
+                    "mp2_correlation_energy": mp2_correlation_energy,
+                    "mp2_total_energy": mp2_total_energy,
+                }
+            )
+        if ccsd_correlation_energy is not None:
+            qcschema_dict["properties"].update(
+                {
+                    "ccsd_correlation_energy": ccsd_correlation_energy,
+                    "ccsd_total_energy": ccsd_total_energy,
+                }
+            )
+
+        # TODO gradient, Hessian
+        qcschema_dict["return_result"] = return_energy
 
         return qcschema_dict
 
