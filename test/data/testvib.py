b0a6e02823850ef17de8aa7ec4e9ce73cbe70050
Eric Berquist <eric.berquist@gmail.com>
2021-01-03 16:29:34 -0500

    GAMESS: parse ZPVE

--- test/data/testvib.py
+++ test/data/testvib.py
@@ -88,7 +88,6 @@ class GenericIRTest(unittest.TestCase):
 
     @skipForParser('ADF', 'not implemented yet')
     @skipForParser('DALTON', 'not implemented yet')
-    @skipForParser('GAMESS', 'not implemented yet')
     @skipForParser('GAMESSUK', 'not implemented yet')
     @skipForParser('Jaguar', 'not implemented yet')
     @skipForParser('Molcas', 'not implemented yet')
