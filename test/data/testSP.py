764173a70eedcf0d1245d4bc68572e7e57cc9ccb
Eric Berquist <eric.berquist@gmail.com>
2021-05-23 23:43:02 -0400

    Remove stray comment

--- test/data/testSP.py
+++ test/data/testSP.py
@@ -241,7 +241,6 @@ class GenericSPTest(unittest.TestCase):
     @skipForParser('Gaussian', 'Logfile needs to be updated')
     @skipForParser('Jaguar', 'No dipole moments in the logfile')
     @skipForParser('Molcas','The parser is still being developed so we skip this test')
-    #@skipForLogfile('Turbomole/basicTurbomole5.9', 'The dipole moment in this log file is not quite equal to 0')
     def testmoments(self):
         """Does the dipole and possible higher molecular moments look reasonable?"""
 
