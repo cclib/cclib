f097f988b0ee3c96df018c41209d3c1d73aac400
Eric Berquist <eric.berquist@gmail.com>
2021-07-05 16:23:41 -0400

    QCSchemaWriter: comment that geometry optimizations are procedures, not drivers

--- test/io/testqcschemawriter.py
+++ test/io/testqcschemawriter.py
@@ -5,7 +5,7 @@
 # This file is part of cclib (http://cclib.github.io) and is distributed under
 # the terms of the BSD 3-Clause License.
 
-"""Unit tests for QCSchema writer."""
+"""Unit tests for the QCSchema writer."""
 
 import os
 import unittest
@@ -32,6 +32,11 @@ class QCSchemaWriterTest(unittest.TestCase):
         validate_output(fpath)
 
     def test_validate_output_b3lyp_gradient(self):
+        # This is not quite right, because a geometry optimization is a
+        # procedure (repeated use of the gradient driver) and not a driver
+        # itself. In the future, when requesting QCSChema output, a geometry
+        # optimization should split into multiple force outputs (might depend
+        # on #657).
         fpath = __datadir__ / "data" / "QChem" / "basicQChem5.1" / "dvb_gopt.out"
         validate_output(fpath)
 
