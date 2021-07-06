b903021721548d75f9bdcf098781dbea3d1200eb
Eric Berquist <eric.berquist@gmail.com>
2021-07-05 22:30:55 -0400

    NMR: update unit test to match updated attribute

--- test/data/testNMR.py
+++ test/data/testNMR.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2017, the cclib development team
+# Copyright (c) 2021, the cclib development team
 #
 # This file is part of cclib (http://cclib.github.io) and is distributed under
 # the terms of the BSD 3-Clause License.
 
-"""Test nmr logfiles in cclib"""
+"""Test NMR logfiles in cclib."""
+
 import os
 import unittest
 
@@ -15,19 +16,19 @@ __filedir__ = os.path.realpath(os.path.dirname(__file__))
 
 
 class GenericNMRTest(unittest.TestCase):
-    """Generic nmr DFT unittest"""
+    """Generic NMR unittest"""
 
     def testsize(self):
         """Check to make sure there are the correct number of tensors parsed"""
-        self.assertEqual(len(self.data.nmrtensors), 3)
-        self.assertEqual(len(self.data.nmrtensors[0]), self.data.natom)
-        self.assertEqual(self.data.nmrtensors[0][0].shape, (3, 3))
+        self.assertEqual(len(self.data.nmrtensors), self.data.natom)
+        self.assertEqual(len(self.data.nmrtensors[0]), 3)
+        self.assertEqual(self.data.nmrtensors[0]["total"].shape, (3, 3))
 
 
 if __name__ == "__main__":
     import sys
 
-    sys.path.append(os.path.join(__filedir__, ".."))
+    sys.path.insert(1, os.path.join(__filedir__, ".."))
 
     from test_data import DataSuite
 
