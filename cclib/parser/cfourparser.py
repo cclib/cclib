9f972186006ceffd9c461de40c53714e83964a49
amandadumi <amanda.e.dumi@gmail.com>
2021-07-28 01:20:56 -0400

    cleaning, use skip_lines

--- cclib/parser/cfourparser.py
+++ cclib/parser/cfourparser.py
@@ -58,87 +58,73 @@ class CFOUR(logfileparser.Logfile):
             #  Symbol    Number           X              Y              Z
             # ----------------------------------------------------------------
             #     O         8         0.00000000     0.00000000     0.00000000
-            for i in range(2):
-                 line = next(inputfile)
+            self.skip_lines(inputfile,['ah','ah'])
             coord_block = numpy.array(self._parse_block(inputfile, self.natom,str, 'parsing coordinate blocks'))
             self.atomnos = numpy.array(coord_block[:,1],dtype=int)
             self.atomcoords = numpy.array(coord_block[:,2:],dtype=float)
 
         # find the number of basis functions
-        ang_mom_map = {'S':'S', 'X':'P', 'XX':'D', 'XXX':'F'}
+        ang_mom_map = {'S':'S', 'X':'P', 'XX':'D','XXX':'F'}
         if  'There are' in line and 'basis functions.' in line:
             self.nbasis = line.split()[2]
         if  'GAUSSIAN BASIS INFORMATION' in line:
-            for i in range(4):
-                 line = next(inputfile)
+            self.skip_lines(inputfile, ['blank', 'column_titles', 'blank'])
+            line = next(inputfile)
             self.gbasis = [[]]
             split_line = line.split() # example line:  O #1  1    S
             atom_num = int(split_line[1].strip('#'))
-            new_atom_num = 0
             ang_mom = split_line[3]
-            basis_done = False
             line = next(inputfile)
-            count = 0
-            new = True
+            new_atom_num = 0
+            new_shell = True
+            basis_done = False
+
             while not basis_done:
-                # data structure to store right shape of coeffs, exp pairs.
+                # Data structure to store right shape of coeffs, exp pairs.
                 overall_basis = []
                 while ('#' not in line) and (basis_done is False):
-                    ## the basis section end is marked by a line with two numbers.
+                    # the basis section end is marked by a line with two numbers.
                     if len(line.split()) == 2:
                         basis_done=True
                         break
                     # there are new lines between each basis shell.
                     if line =='\n':
                         line = next(inputfile)
-                        new = True
+                        new_shell = True
                         continue
-                    if ang_mom not in ['S','X','XX','XXX']: # CFOUR outputs each component, though cclib only stores as S, P, D.
+                    # CFOUR outputs each component, cclib only stores as 1 per ang_mom
+                    if ang_mom not in ['S','X','XX','XXX']:
                         line = next(inputfile)
                         while ('#' not in line):
                             line = next(inputfile)
                             if len(line) == 2:
                                 basis_done = True
-                        print(line)
                     else:
-                        line = line.strip('+')
-                        split_line = line.split()
-                        exp = float(split_line[1])
-                        coeffs = numpy.array(split_line[2:],dtype=float)
-                        print(numpy.shape(overall_basis))
-                        print(len(overall_basis))
+                        line = line.strip('+').split()
+                        exp = float(line[1])
+                        coeffs = numpy.array(line[2:],dtype=float)
                         for idx, i in enumerate(coeffs):
-                            # if you are starting on a new atom, or starting a new angular momenta shell.
-                            print('new is {}'.format(new))
-                            if new == True or len(overall_basis) ==0:
-                                print('we are here')
+                            # if you are starting on a new atom, or starting
+                            # a new angular momenta shell.
+                            if new_shell == True or len(overall_basis) == 0:
                                 overall_basis.append([(exp,i)])
                                 if idx+1 == len(coeffs):
-                                    new = False
-                            # you are in the same shell
+                                    new_shell = False
+                            # In the same shell
                             else:
-                                print('now we are here!')
                                 overall_basis[idx].append((exp,i))
-                        # pairs = zip(numpy.ones(len(coeffs))*exp,coeffs)
                         line = next(inputfile)
-                        # overall_basis.concatenate(basis_list,axis=1)
                 for i in range(len(overall_basis)):
                     self.gbasis[-1].append((ang_mom_map[ang_mom],overall_basis[i]))
                 if basis_done:
                     break
-                print('line after appending new basis is:')
-                print(line)
                 split_line = line.split() # example line:  O #1  1    S
                 new_atom_num = int(split_line[1].strip('#'))
                 ang_mom = split_line[3]
-                line = next(inputfile)
                 if new_atom_num != atom_num:
                     self.gbasis.append([])
                     atom_num = new_atom_num
-                count +=1
-                if count == 3:
-                    exit
-                print(self.gbasis)
+                line = next(inputfile)
         else:
             pass
             # raise Warning('CFOUR job was not run with PRINT=1 option, basis is not detailed enough')
