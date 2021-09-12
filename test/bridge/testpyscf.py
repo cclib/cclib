60dfc184c2beb154414cf63abe89841f45cae7ed
Eric Berquist <eric.berquist@gmail.com>
2021-09-12 16:53:28 -0400

    PySCF bridge now produces energies in hartree

--- test/bridge/testpyscf.py
+++ test/bridge/testpyscf.py
@@ -44,7 +44,7 @@ class PyscfTest(unittest.TestCase):
     def test_makepyscf_mos(self) -> None:
         pyscfmol = cclib2pyscf.makepyscf(self.data)
         mo_coeff, mo_occ, mo_syms, mo_energies = cclib2pyscf.makepyscf_mos(self.data, pyscfmol)
-        assert np.allclose(mo_energies, convertor(np.array(self.data.moenergies), "eV", "hartree"))
+        assert np.allclose(mo_energies, self.data.moenergies)
         # check first MO coefficient
         assert np.allclose(mo_coeff[0][0], self.data.mocoeffs[0][0][0])
         # check a random middle MO coefficient
@@ -52,7 +52,7 @@ class PyscfTest(unittest.TestCase):
         # test unrestricted code.
         pyscfmol = cclib2pyscf.makepyscf(self.udata)
         mo_coeff, mo_occ, mo_syms, mo_energies = cclib2pyscf.makepyscf_mos(self.udata, pyscfmol)
-        assert np.allclose(mo_energies, convertor(np.array(self.udata.moenergies), "eV", "hartree"))
+        assert np.allclose(mo_energies, self.udata.moenergies)
         # check first MO coefficient
         assert np.allclose(mo_coeff[0][0][0], self.udata.mocoeffs[0][0][0])
         # check a random middle MO coefficient
