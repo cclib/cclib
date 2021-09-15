d352b81f2b831d57c2055bfa72e7fb32f2fafb4b
amandadumi <amanda.e.dumi@gmail.com>
2021-09-14 23:43:51 -0400

    consider symmetry, still tests failing

--- cclib/parser/cfourparser.py
+++ cclib/parser/cfourparser.py
@@ -20,8 +20,7 @@ from cclib.parser import utils
 
 
 class CFOUR(logfileparser.Logfile):
-    """ A CFOUR output file
-    """
+    """A CFOUR output file"""
 
     def __init__(self, *args, **kwargs):
         # Call the __init__ method of the superclass
@@ -40,17 +39,16 @@ class CFOUR(logfileparser.Logfile):
         """Just return label"""
         return symlabel
 
-
     def extract(self, inputfile, line):
         # just opened file, skip first line to get basis
-        if 'Version' in line:
+        if "Version" in line:
             package_version = line.split()[1]
             self.metadata["package_version"] = package_version
 
-        if '@GETXYZ-I' in line:
+        if "@GETXYZ-I" in line:
             self.natom = int(line.split()[1])
 
-        if 'Coordinates (in bohr)' in line:
+        if "Coordinates (in bohr)" in line:
             # skip labeling lines
             #         Coordinates used in calculation (QCOMP)
             # ----------------------------------------------------------------
@@ -58,79 +56,105 @@ class CFOUR(logfileparser.Logfile):
             #  Symbol    Number           X              Y              Z
             # ----------------------------------------------------------------
             #     O         8         0.00000000     0.00000000     0.00000000
-            self.skip_lines(inputfile,['ah','ah'])
-            coord_block = numpy.array(self._parse_block(inputfile, self.natom,str, 'parsing coordinate blocks'))
-            self.atomnos = numpy.array(coord_block[:,1],dtype=int)
-            self.atomcoords = numpy.array(coord_block[:,2:],dtype=float)
+            self.skip_lines(inputfile, ["ah", "ah"])
+            coord_block = numpy.array(
+                self._parse_block(
+                    inputfile, self.natom, str, "parsing coordinate blocks"
+                )
+            )
+            self.atomnos = numpy.array(coord_block[:, 1], dtype=int)
+            self.atomcoords = numpy.array(coord_block[:, 2:], dtype=float)
 
         # find the number of basis functions
-        ang_mom_map = {'S':'S', 'X':'P', 'XX':'D','F300':'F','G400':'G'}
-        if  'There are' in line and 'basis functions.' in line:
+        # check to see if atom symmetry is accounted for:
+        ang_mom_map = {"S": "S", "X": "P", "XX": "D", "F300": "F", "G400": "G"}
+        if "There are" in line and "basis functions." in line:
             self.nbasis = int(line.split()[2])
-        if  'GAUSSIAN BASIS INFORMATION' in line:
-            self.skip_lines(inputfile, ['blank', 'column_titles', 'blank'])
+
+        if "TYPES OF ATOMS" in line:
+            self.sym_atom_vec = None
+            sym_atom_types = int(line.split()[0])
+            if sym_atom_types != self.natom:
+                print("we are herrrreee even if we shouldne be")
+                while "INTERNUCLEAR DISTANCES (A) :" not in line:
+                    line = next(inputfile)
+                if "INTERNUCLEAR DISTANCES (A) :" in line:
+                    self.skip_lines(inputfile, ["blank"])
+                    line = next(inputfile)
+                    self.sym_atom_vec = [int(line.split()[3].strip("#"))]
+                    self.skip_lines(inputfile, ["blank"])
+                    for i in range(self.natom - 1):
+                        line = next(inputfile)
+                        anum = int(line.split()[1].strip("#"))
+                        # store the type of each atom for the ordering in input coordinates.
+                        self.sym_atom_vec.append(anum)
+
+        if "GAUSSIAN BASIS INFORMATION" in line:
+            self.skip_lines(inputfile, ["blank", "column_titles", "blank"])
             line = next(inputfile)
-            self.gbasis = [[]]
-            split_line = line.split() # example line:  O #1  1    S
-            atom_num = int(split_line[1].strip('#'))
+            self.sym_unique_gbasis = [[]]
+            split_line = line.split()  # example line:  O #1  1    S
+            atom_num = int(split_line[1].strip("#"))
             ang_mom = split_line[3]
             line = next(inputfile)
             new_atom_num = 0
             new_shell = True
             basis_done = False
-
             while not basis_done:
                 # Data structure to store right shape of coeffs, exp pairs.
                 overall_basis = []
-                while ('#' not in line) and (basis_done is False):
+                while ("#" not in line) and (basis_done is False):
                     # the basis section end is marked by a line with two numbers.
-                    if len(line.split()) == 2 and ('.' not in line):
-                        basis_done=True
+                    if len(line.split()) == 2 and ("." not in line):
+                        basis_done = True
                         break
                     # there are new lines between each basis shell.
-                    if line =='\n':
+                    if line == "\n":
                         line = next(inputfile)
                         new_shell = True
                         continue
                     # CFOUR outputs each component, cclib only stores as 1 per ang_mom
-                    if ang_mom not in ['S','X','XX','XXX','F300','G400']:
+                    if ang_mom not in ["S", "X", "XX", "XXX", "F300", "G400"]:
                         line = next(inputfile)
-                        while ('#' not in line):
+                        while "#" not in line:
                             line = next(inputfile)
                             if len(line) == 2:
                                 basis_done = True
                     else:
-                        line = line.strip('+').split()
+                        line = line.strip("+").split()
                         exp = float(line[1])
-                        coeffs = numpy.array(line[2:],dtype=float)
+                        coeffs = numpy.array(line[2:], dtype=float)
                         for idx, i in enumerate(coeffs):
                             # if you are starting on a new atom, or starting
                             # a new angular momenta shell.
                             if new_shell == True or len(overall_basis) == 0:
-                                overall_basis.append([(exp,i)])
-                                if idx+1 == len(coeffs):
+                                overall_basis.append([(exp, i)])
+                                if idx + 1 == len(coeffs):
                                     new_shell = False
                             # In the same shell
                             else:
-                                overall_basis[idx].append((exp,i))
+                                overall_basis[idx].append((exp, i))
                         line = next(inputfile)
                 for i in range(len(overall_basis)):
-                    self.gbasis[-1].append((ang_mom_map[ang_mom],overall_basis[i]))
+                    self.sym_unique_gbasis[-1].append(
+                        (ang_mom_map[ang_mom], overall_basis[i])
+                    )
                 if basis_done:
                     break
-                split_line = line.split() # example line:  O #1  1    S
-                new_atom_num = int(split_line[1].strip('#'))
+                split_line = line.split()  # example line:  O #1  1    S
+                new_atom_num = int(split_line[1].strip("#"))
                 ang_mom = split_line[3]
                 if new_atom_num != atom_num:
-                    self.gbasis.append([])
+                    self.sym_unique_gbasis.append([])
                     atom_num = new_atom_num
                 line = next(inputfile)
-        else:
-            pass
-            # raise Warning('CFOUR job was not run with PRINT=1 option, basis is not detailed enough')
-
-
-
+            # If the molecule has symmetry, this should expand the gbasis object to have an entry for every atom, not just the symmetry unique ones.
+            self.gbasis = []
+            if self.sym_atom_vec is not None:
+                for a in self.sym_atom_vec:
+                    self.gbasis.append(self.sym_unique_gbasis[a])
+            else:
+                self.gbasis = self.sym_unique_gbasis
 
     def _parse_block(self, inputfile, count, type, msg):
         """
@@ -147,10 +171,8 @@ class CFOUR(logfileparser.Logfile):
           message to pass to the update log.
         """
         property = []
-        while len(property) < count :
+        while len(property) < count:
             self.updateprogress(inputfile, msg, self.fupdate)
             line = next(inputfile)
-            property.append([ type(x) for x in line.split()])
-            print(property)
-        print(numpy.shape(property))
+            property.append([type(x) for x in line.split()])
         return property
