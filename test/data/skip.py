cf5a344fa691931c6da924b3b54efc74fbfdbdd8
Shiv Upadhyay <shivnupadhyay@gmail.com>
2022-11-29 19:52:07 -0500

    add test decorator

--- test/data/skip.py
+++ test/data/skip.py
@@ -5,6 +5,8 @@
 # This file is part of cclib (http://cclib.github.io) and is distributed under
 # the terms of the BSD 3-Clause License.
 
+import sys
+
 """Tools for skipping data tests in cclib."""
 
 
@@ -31,3 +33,27 @@ def skipForLogfile(fragment, msg):
                 testfunc(self, *args, **kwargs)
         return testwrapper
     return testdecorator
+
+def skipForVersion(version, msg):
+    """Return a decorator that skips the test for specified python version."""
+    def testdecorator(testfunc):
+        def testwrapper(self, *args, **kwargs):
+            curr_ver = sys.version_info
+            skip_test = True
+            if "major" in version:
+                if version["major"] != curr_ver.major:
+                    skip_test = False
+            if "minor" in version:
+                if version["minor"] != curr_ver.minor:
+                    skip_test = False
+            if "micro" in version:
+                if version["micro"] != curr_ver.micro:
+                    skip_test = False
+            if skip_test:
+                self.skipTest(msg)
+            else:
+                testfunc(self, *args, **kwargs)
+        return testwrapper
+    return testdecorator
+
+
