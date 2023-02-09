f0ca744537fdb53cfbcab99e81dc1e70bc702728
oliver-s-lee <osl@st-andrews.ac.uk>
2023-02-09 09:42:25 +0000

    Fixed regression tests for wall_time

--- test/regression.py
+++ test/regression.py
@@ -770,8 +770,7 @@ def testGaussian_Gaussian98_test_Cu2_log(logfile):
     """An example of the number of basis set function changing."""
     assert logfile.data.nbasis == 38
 
-    assert logfile.data.metadata["cpu_time"] == [datetime.timedelta(seconds=25, microseconds=800000)]
-    assert "wall_time" not in logfile.data.metadata
+    assert logfile.data.metadata["cpu_time"] == logfile.data.metadata["wall_time"] == [datetime.timedelta(seconds=25, microseconds=800000)]
     assert logfile.data.metadata["legacy_package_version"] == "98revisionA.11.4"
     assert logfile.data.metadata["package_version"] == "1998+A.11.4"
     assert isinstance(
@@ -882,8 +881,8 @@ def testGaussian_Gaussian03_dvb_gopt_symmfollow_log(logfile):
     """
     assert len(logfile.data.atomcoords) == len(logfile.data.geovalues)
 
-    assert logfile.data.metadata["cpu_time"] == [datetime.timedelta(seconds=99)]
-    assert "wall_time" not in logfile.data.metadata
+    # This calc only uses one CPU, so wall_time == cpu_time.
+    assert logfile.data.metadata["cpu_time"] == logfile.data.metadata["wall_time"]== [datetime.timedelta(seconds=99)]
     assert logfile.data.metadata["legacy_package_version"] == "03revisionC.01"
     assert logfile.data.metadata["package_version"] == "2003+C.01"
     assert isinstance(
@@ -1000,8 +999,7 @@ def testGaussian_Gaussian09_dvb_gopt_unconverged_log(logfile):
     assert hasattr(logfile.data, 'optdone') and not logfile.data.optdone
     assert logfile.data.optstatus[-1] == logfile.data.OPT_UNCONVERGED
 
-    assert logfile.data.metadata["cpu_time"] == [datetime.timedelta(seconds=27, microseconds=700000)]
-    assert "wall_time" not in logfile.data.metadata
+    assert logfile.data.metadata["cpu_time"] == logfile.data.metadata["wall_time"] == [datetime.timedelta(seconds=27, microseconds=700000)]
     assert logfile.data.metadata["package_version"] == "2009+D.01"
 
 
