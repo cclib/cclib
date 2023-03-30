ca7d1ce4823a92318c69c548ad47453ef53b8e09
oliver-s-lee <osl@st-andrews.ac.uk>
2023-03-30 16:13:19 +0100

    Added test to ensure nmr dict keys are integers and rearranged nmr tests a bit

--- test/data/testNMR.py
+++ test/data/testNMR.py
@@ -19,6 +19,11 @@ __filedir__ = os.path.realpath(os.path.dirname(__file__))
 
 class GenericNMRTest(unittest.TestCase):
     """Generic NMR unittest"""
+    
+    def testkeys(self):
+        """Check dictionary keys are ints."""
+        key_types = set([type(key) for key in self.data.nmrtensors.keys()])
+        assert len(key_types) == 1 and int in key_types
 
     def testsize(self):
         """Check to make sure there are the correct number of tensors parsed"""
@@ -26,8 +31,9 @@ class GenericNMRTest(unittest.TestCase):
         assert len(self.data.nmrtensors[0]) == 4
         assert self.data.nmrtensors[0]["total"].shape == (3, 3)
         
+    def testisotropic(self):
+        """Check the total isotropic value matches the computed value."""
         tensor = self.data.nmrtensors[0]
-        # Check the total isotropic value matches the computed value.
         total = 0.0
         for t_type in ("diamagnetic", "paramagnetic"):
             total += sum(numpy.linalg.eigvals(tensor[t_type])) / len(numpy.linalg.eigvals(tensor[t_type]))
@@ -37,15 +43,29 @@ class GenericNMRTest(unittest.TestCase):
 
 class GenericNMRCouplingTest(unittest.TestCase):
     """Generic NMR spin-spin coupling unittest"""
+    
+    def testkeys(self):
+        """Check dictionary keys are ints."""
+        assert all(set([all((type(key[0]) == int, type(key[1]) == int)) for key in self.data.nmrcouplingtensors.keys()]))
+        
+        assert all(
+            [
+                all((type(isotropekey[0]) == int, type(isotropekey[1]) == int))
+                for isotropes in self.data.nmrcouplingtensors.values()
+                for isotropekey in isotropes.keys()
+            ]
+        )
 
     def testsize(self):
         """Check to make sure there are the correct number of tensors parsed"""
         assert len(self.data.nmrcouplingtensors) == 139
-        
         tensor = list(list(self.data.nmrcouplingtensors.values())[0].values())[0]
         assert len(tensor) == 7
         assert tensor['total'].shape == (3, 3)
-        
+    
+    def testisotropic(self):
+        """Check the total isotropic value matches the computed value."""
+        tensor = list(list(self.data.nmrcouplingtensors.values())[0].values())[0]
         # Check the total isotropic value matches the computed value.
         total = 0.0
         for t_type in ("diamagnetic", "paramagnetic", "fermi", "spin-dipolar", "spin-dipolar-fermi"):
