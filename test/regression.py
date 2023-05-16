4a928241f2debcba60bcff1c862682455ef05d0b
Shiv Upadhyay <shivnupadhyay@gmail.com>
2023-05-16 08:45:33 -0400

    update function call in regression

--- test/regression.py
+++ test/regression.py
@@ -764,7 +764,7 @@ def testnoparseGAMESS_WinGAMESS_H2O_def2SVPD_triplet_2019_06_30_R1_out(filename)
     """
     data = ccread(os.path.join(__filedir__,filename))
     writer = moldenwriter.MOLDEN(data)
-    mosyms, mocoeffs, mooccs, moenergies = writer._syms_coeffs_occs_energies_from_ccdata_for_moldenwriter()
+    mosyms, mocoeffs, mooccs, moenergies = writer._syms_energies_occs_coeffs_from_ccdata_for_moldenwriter()
     molden_lines = writer._mo_from_ccdata(mosyms,moenergies,mocoeffs,mooccs)
     # Check size of Atoms section.
     assert len(molden_lines) == (data.nbasis + 4) * (data.nmo * 2)
