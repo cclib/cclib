2ba1ed92f28a918892a9dd2ed2fe9a810e0968ea
weronikazak <weronikarzak@gmail.com>
2023-06-01 00:01:02 +0100

    Extract energy

--- cclib/parser/gamessdatparser.py
+++ cclib/parser/gamessdatparser.py
@@ -9,9 +9,7 @@
 
 
 import re
-
 import numpy
-
 
 from cclib.parser import logfileparser
 from cclib.parser import utils
@@ -41,3 +39,45 @@ class GAMESSDAT(logfileparser.Logfile):
 
     def extract(self, inputfile, line):
         """Extract information from the file object inputfile."""
+        
+        # Extract element and its properties
+
+        # $DATA  
+        # water                                                                           
+        # C1       0
+        # O           8.0      0.0000000000      0.0000000000      0.0000000000
+        #    STO     3
+                
+        # H           1.0      0.9900000000      0.0000000000      0.0000000000
+        #    STO     3
+                
+        # H           1.0     -0.2728810000      0.9516490000      0.0000000000
+        #    STO     3
+                
+        #  $END   
+
+        # TODO
+
+
+        # Extract energy
+
+        # --- CLOSED SHELL ORBITALS --- GENERATED AT Mon Aug  5 13:05:47 2019
+        # water                                                                           
+        # E(RHF)=      -74.9643287920, E(NUC)=    8.8870072224,   13 ITERS
+
+        # Extract E(RHF) value using regex
+        rhf_match = re.search(r"E\(RHF\)=(.*?),", line)
+        if rhf_match:
+            self.metadata["E_RHF"] = rhf_match.group(1).strip()
+
+        # Extract E(NUC) value using regex
+        nuc_match = re.search(r"E\(NUC\)=(.*?),", line)
+        if nuc_match:
+            self.metadata["E_NUC"] = nuc_match.group(1).strip()
+
+        # Extract number of ITERS using regex
+        iters_match = re.search(r"(\d+)\s+ITERS", line)
+        if iters_match:
+            self.metadata["ITERS"] = iters_match.group(1).strip()
+
+        
