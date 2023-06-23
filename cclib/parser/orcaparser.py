759a41bbba572efb3f551e011e8d5d1d91b8c985
oliver-s-lee <osl@st-andrews.ac.uk>
2023-06-23 16:57:45 +0100

    Added parsing of post-HF excited state method for metadata (Orca)

--- cclib/parser/orcaparser.py
+++ cclib/parser/orcaparser.py
@@ -1546,6 +1546,15 @@ States  Energy Wavelength    D2        m2        Q2         D2+m2+Q2       D2/TO
             self.mdci_et_mult = line.split()[-1].capitalize()
             
         if any(x in line for x in ("CIS RESULTS", "ADC(2) RESULTS", "EOM-CCSD RESULTS", "STEOM-CCSD RESULTS")):
+            if "ADC(2)" in line:
+                self.metadata['excited_states_method'] = "ADC(2)"
+            
+            elif "STEOM-CCSD RESULTS" in line:
+                self.metadata['excited_states_method'] = "STEOM-CCSD"
+                
+            elif "EOM-CCSD RESULTS" in line:
+                self.metadata['excited_states_method'] = "EOM-CCSD"
+            
             if self.mdci_et_mult is None and ( "EOM-CCSD" in line or "ADC(2)" in line ):
                 # These methods can only do singlets.
                 # Think this is safe?.
