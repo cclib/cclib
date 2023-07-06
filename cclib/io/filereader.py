c39839ae1c9f7e363df13d89fea192538dd8f934
oliver-s-lee <osl@st-andrews.ac.uk>
2023-07-06 11:24:09 +0100

    FileReaders now support FileWrapper

--- cclib/io/filereader.py
+++ cclib/io/filereader.py
@@ -8,12 +8,15 @@
 """Generic file reader and related tools"""
 
 from abc import ABC, abstractmethod
+import typing
+
+from cclib.parser.logfilewrapper import FileWrapper
 
 
 class Reader(ABC):
     """Abstract class for reader objects."""
 
-    def __init__(self, source: str, *args, **kwargs) -> None:
+    def __init__(self, source: str | typing.io | FileWrapper, *args, **kwargs) -> None:
         """Initialize the Reader object.
 
         This should be called by a subclass in its own __init__ method.
@@ -21,18 +24,14 @@ class Reader(ABC):
         Inputs:
           source - A single filename, stream [TODO], or list of filenames/streams [TODO].
         """
-        if isinstance(source, str):
-            self.filename = source
-        else:
-            raise ValueError
+        if not isinstance(source, FileWrapper):
+            source = FileWrapper(source)
+        
+        self.file = source
 
     def parse(self) -> None:
         """Read the raw contents of the source into the Reader."""
-        # TODO This cannot currently handle streams.
-        with open(self.filename) as handle:
-            self.filecontents = handle.read()
-
-        return None
+        self.filecontents = self.file.read()
 
     @abstractmethod
     def generate_repr(self) -> None:
