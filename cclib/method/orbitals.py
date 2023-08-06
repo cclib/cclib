5ec524950318f96c95044d388412f538bfa12baa
Eric Berquist <eric.berquist@gmail.com>
2023-08-06 17:42:12 -0400

    Fix headers to use BSD 3-clause license rather than LGPL 2.1

--- cclib/method/orbitals.py
+++ cclib/method/orbitals.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of cclib (http://cclib.github.io), a library for parsing
-# and interpreting the results of computational chemistry packages.
+# Copyright (c) 2023, the cclib development team
 #
-# Copyright (C) 2017, the cclib development team
-#
-# The library is free software, distributed under the terms of
-# the GNU Lesser General Public version 2.1 or later. You should have
-# received a copy of the license along with cclib. You can also access
-# the full license online at http://www.gnu.org/copyleft/lgpl.html.
+# This file is part of cclib (http://cclib.github.io) and is distributed under
+# the terms of the BSD 3-Clause License.
 
 """Analyses related to orbitals."""
 
