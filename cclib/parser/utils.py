e697d85e486e6fd34319cef233f120a68628db4d
Eric Berquist <eric.berquist@gmail.com>
2023-08-06 17:47:19 -0400

    Update copyright year to 2023

--- cclib/parser/utils.py
+++ cclib/parser/utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2020, the cclib development team
+# Copyright (c) 2023, the cclib development team
 #
 # This file is part of cclib (http://cclib.github.io) and is distributed under
 # the terms of the BSD 3-Clause License.
