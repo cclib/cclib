Fix nre calculation

cclib/method/nuclear.py
@@ -103,15 +103,15 @@ class Nuclear(Method):
     def repulsion_energy(self, atomcoords_index: int = -1) -> float:
         """Return the nuclear repulsion energy."""
         nre = 0.0
+        atomcoords = convertor(self.data.atomcoords[atomcoords_index], "Angstrom", "bohr")
         for i in range(self.data.natom):
-            atomcoords = self.data.atomcoords[atomcoords_index]
             ri = atomcoords[i]
             zi = self.data.atomnos[i]
             for j in range(i+1, self.data.natom):
                 rj = atomcoords[j]
                 zj = self.data.atomnos[j]
                 d = np.linalg.norm(ri-rj)
-                nre += zi * zj / convertor(d, "Angstrom", "bohr")
+                nre += zi * zj / d
         return convertor(nre, "hartree", "eV")
 
     def center_of_mass(self, atomcoords_index: int = -1) -> float: