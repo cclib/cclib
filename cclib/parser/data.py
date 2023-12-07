e29d34f5ad139568a4cb5514ce8709d7e739be5e
Eric Berquist <eric.berquist@gmail.com>
2023-12-06 23:43:02 -0500

    make optdone-as-list the default

--- cclib/parser/data.py
+++ cclib/parser/data.py
@@ -444,19 +444,3 @@ class ccData:
     @property
     def closed_shell(self) -> bool:
         return orbitals.Orbitals(self).closed_shell()
-
-
-class ccData_optdone_bool(ccData):
-    """This is the version of ccData where optdone is a Boolean."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._attributes["optdone"] = Attribute(bool, "done", "optimization")
-
-    def setattributes(self, *args, **kwargs):
-        invalid = super().setattributes(*args, **kwargs)
-
-        # Reduce optdone to a Boolean, because it will be parsed as a list. If this list has any element,
-        # it means that there was an optimized structure and optdone should be True.
-        if hasattr(self, "optdone"):
-            self.optdone = len(self.optdone) > 0
