bc830f97b0200daf5574570b655dc00009d14176
Amanda Dumi <amanda.e.dumi@gmail.com>
2024-01-03 19:59:00 -0700

    removing unneeded loop

--- cclib/parser_properties/base_parser.py
+++ cclib/parser_properties/base_parser.py
@@ -10,11 +10,10 @@ class base_parser(ABC):
 
     @staticmethod
     def check_dependencies(dependency_list, ccdata, current_property):
-        if len(dependency_list) > 0:
-            for i in dependency_list:
-                if not hasattr(ccdata, i):
-                    raise Warning(
-                        f"to parse {current_property}, ccdata required dependency of {i} which is not present"
-                    )
-                    return False
+        for i in dependency_list:
+             if not hasattr(ccdata, i):
+                  raise Warning(
+                      f"to parse {current_property}, ccdata required dependency of {i} which is not present"
+                  )
+                  return False
         return True
