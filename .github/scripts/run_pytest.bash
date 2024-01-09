6684e6fdb44cc9b44a33458629228e18c74015a6
Eric Berquist <eric.berquist@gmail.com>
2024-01-09 00:31:51 -0500

    use pytest-xdist in CI

--- .github/scripts/run_pytest.bash
+++ .github/scripts/run_pytest.bash
@@ -3,11 +3,19 @@
 # run_pytest.bash: Run pytest on cclib with coverage checking. Requires
 # `pytest` and `pytest-cov`.
 
-set -euxo pipefail
+set -exo pipefail
 
-PYTEST_ADDOPTS='-v -s --cov=cclib --cov-report=term --cov-report=xml:coverage-unit.xml -m "not is_method"' python -m pytest
+PYTEST_NWORKERS="${1}"
+
+if [[ -n "${PYTEST_NWORKERS}" ]]; then
+    PYTEST_PARALLELISM="-n ${PYTEST_NWORKERS} --dist worksteal"
+else
+    PYTEST_PARALLELISM=""
+fi
+
+PYTEST_ADDOPTS="-v -s ${PYTEST_PARALLELISM} --cov=cclib --cov-report=term --cov-report=xml:coverage-unit.xml -m 'not is_method'" python -m pytest
 pushd data
 bash ./regression_download.sh
 popd
-PYTEST_ADDOPTS='-v -s --cov=cclib --cov-report=term --cov-report=xml:coverage-regression.xml --cov-append' python -m pytest test/regression.py
-PYTEST_ADDOPTS='-v -s --cov=cclib --cov-report=term --cov-report=xml:coverage-regression.xml --cov-append ' python -m pytest test/regression_io.py
+PYTEST_ADDOPTS="-v -s ${PYTEST_PARALLELISM} --cov=cclib --cov-report=term --cov-report=xml:coverage-regression.xml --cov-append" python -m pytest test/regression.py
+PYTEST_ADDOPTS="-v -s ${PYTEST_PARALLELISM} --cov=cclib --cov-report=term --cov-report=xml:coverage-regression.xml --cov-append" python -m pytest test/regression_io.py
--- .github/scripts/run_pytest_methods.bash
+++ .github/scripts/run_pytest_methods.bash
@@ -3,6 +3,14 @@
 # run_pytest.bash: Run pytest on cclib with coverage checking. Requires
 # `pytest` and `pytest-cov`.
 
-set -euxo pipefail
+set -exo pipefail
 
-PYTEST_ADDOPTS='-v -s --cov=cclib --cov-report=term --cov-report=xml:coverage-method.xml -m "is_method"' python -m pytest
+PYTEST_NWORKERS="${1}"
+
+if [[ -n "${PYTEST_NWORKERS}" ]]; then
+    PYTEST_PARALLELISM="-n ${PYTEST_NWORKERS} --dist worksteal"
+else
+    PYTEST_PARALLELISM=""
+fi
+
+PYTEST_ADDOPTS="-v -s ${PYTEST_PARALLELISM} --cov=cclib --cov-report=term --cov-report=xml:coverage-method.xml -m 'is_method'" python -m pytest
