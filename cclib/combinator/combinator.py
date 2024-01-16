92f770a512004e273431fe944751af0bede8174a
Amanda Dumi <amanda.e.dumi@gmail.com>
2024-01-16 01:57:51 -0500

    fixing skip lines and adding atombasis for psi4

--- cclib/combinator/combinator.py
+++ cclib/combinator/combinator.py
@@ -17,7 +17,7 @@ class sp_combinator(combinator):
             [
                 cprops.scfenergies,
                 cprops.atomcoords,
-                cprops.atomnos,
+                cprops.atombasis,
                 cprops.atomnos,
                 cprops.charge,
                 cprops.mult,
