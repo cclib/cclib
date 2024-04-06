7017c41c5e6e84ad0e5ab3509fdd35e2fa8b69d8
Eric Berquist <eric.berquist@gmail.com>
2024-04-06 19:30:23 -0400

    test geometry optimizations with atomic units

--- test/data/testGeoOpt.py
+++ test/data/testGeoOpt.py
@@ -23,8 +23,8 @@ class GenericGeoOptTest:
     extrascfs = 0
 
     # Approximate B3LYP energy of dvb after SCF in STO-3G.
-    b3lyp_energy = -10365
-    b3lyp_tolerance = 40
+    b3lyp_energy = -380.90674109218116
+    b3lyp_tolerance = 1.4699729516340807
 
     @skipForParser("Molcas", "The parser is still being developed so we skip this test")
     @skipForParser("MOPAC", "The success status is not parsed yet")
@@ -231,8 +231,8 @@ class ADFGeoOptTest(GenericGeoOptTest):
     extracoords = 1
     extrascfs = 1
 
-    b3lyp_energy = -140
-    b3lyp_tolerance = 1
+    b3lyp_energy = -5.144905330719283
+    b3lyp_tolerance = 0.03674932379085202
 
 
 class DALTONGeoOptTest(GenericGeoOptTest):
@@ -307,8 +307,8 @@ class MOPACGeoOptTest(GenericGeoOptTest):
     """Customized geometry optimization unittest for MOPAC."""
 
     # The geometry optimization unit test logfile uses a PM7 Hamiltonian.
-    b3lyp_energy = 2.22
-    b3lyp_tolerance = 0.01
+    b3lyp_energy = 0.08166294233283113
+    b3lyp_tolerance = 1.6e-5
 
 
 class NWChemGeoOptTest(GenericGeoOptTest):
@@ -389,7 +389,7 @@ class XTBGeoOptTest(GenericGeoOptTest):
 
     def testscfenergy(self, data) -> None:
         """Is the SCF energy within the target?"""
-        assert abs(data.scfenergies[-1] - -719.42119585) < 1.0e-6
+        assert abs(data.scfenergies[-1] - -26.438242468348) < 1.0e-6
 
 
 class TurbomoleKeepGeoOptTest(GenericGeoOptTest):
