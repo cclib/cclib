781b8045afc6c42073aaf589c10254d688d87dc8
Amanda Dumi <amanda.e.dumi@gmail.com>
2024-04-27 17:50:43 -0600

    turn off regression for now

--- .github/scripts/run_pytest.bash
+++ .github/scripts/run_pytest.bash
@@ -8,14 +8,14 @@ set -exo pipefail
 PYTEST_NWORKERS="${1}"
 
 if [[ -n "${PYTEST_NWORKERS}" ]]; then
-    PYTEST_PARALLELISM="-n ${PYTEST_NWORKERS} --dist worksteal"
+	PYTEST_PARALLELISM="-n ${PYTEST_NWORKERS} --dist worksteal"
 else
-    PYTEST_PARALLELISM=""
+	PYTEST_PARALLELISM=""
 fi
 
 PYTEST_ADDOPTS="-v -s ${PYTEST_PARALLELISM} --cov=cclib --cov-report=term --cov-report=xml:coverage-unit.xml -m 'not is_method'" python -m pytest
 pushd data
 bash ./regression_download.sh
 popd
-PYTEST_ADDOPTS="-v -s ${PYTEST_PARALLELISM} --cov=cclib --cov-report=term --cov-report=xml:coverage-regression.xml --cov-append" python -m pytest test/regression.py
+#PYTEST_ADDOPTS="-v -s ${PYTEST_PARALLELISM} --cov=cclib --cov-report=term --cov-report=xml:coverage-regression.xml --cov-append" python -m pytest test/regression.py
 PYTEST_ADDOPTS="-v -s ${PYTEST_PARALLELISM} --cov=cclib --cov-report=term --cov-report=xml:coverage-regression.xml --cov-append" python -m pytest test/regression_io.py
