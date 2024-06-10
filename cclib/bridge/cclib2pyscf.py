1ddbb405dd17a486add22897cf0852c8ca001be2
oliver-s-lee <osl@st-andrews.ac.uk>
2024-06-10 07:59:50 +0100

    switched to better symmetry detection for pyscf

--- cclib/bridge/cclib2pyscf.py
+++ cclib/bridge/cclib2pyscf.py
@@ -239,7 +239,8 @@ def _makecclib(
     attributes["nmo"] = len(attributes["moenergies"][0])
     
     # Orbital symmetries.
-    if scf.mol.symmetry:
+    #if scf.mol.symmetry:
+    if hasattr(scf, 'get_orbsym'):
         # Symmetry labels are in scf.mol.irrep_name, symmetry ids are in scf.mol.irrep_id
         symmetry_mapping = {symm_id: symm_name for symm_id, symm_name in zip(scf.mol.irrep_id, scf.mol.irrep_name)}
         
