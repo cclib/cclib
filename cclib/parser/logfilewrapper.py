6546cebcb15109bf1ec4de27ac93b01c3a7b2725
Eric Berquist <eric.berquist@gmail.com>
2024-06-26 09:33:59 -0400

    enforce FileWrappers as iterators over strings

--- cclib/parser/logfilewrapper.py
+++ cclib/parser/logfilewrapper.py
@@ -15,6 +15,7 @@ import pathlib
 import re
 import typing
 import zipfile
+from collections.abc import Iterator
 from tempfile import NamedTemporaryFile
 from urllib.error import URLError
 from urllib.request import urlopen
@@ -45,7 +46,7 @@ def logerror(error):
 codecs.register_error("logerror", logerror)
 
 
-class FileWrapper:
+class FileWrapper(Iterator[str]):
     """Wrap any supported input file type."""
 
     def __init__(self, *sources) -> None:
@@ -222,20 +223,23 @@ class FileWrapper:
         """
         self.close()
 
-    def next(self) -> str:
+    def __next__(self) -> str:
         """
         Get the next line from this log file.
         """
         try:
             try:
+                # If this throws StopIteration, move to the next file.
+                # If this throws IndexError, we've exhausted all files and are really done.
                 line = next(self.files[self.file_pointer])
                 self.last_lines.append(line)
                 self.pos += len(line)
                 return line
 
             except StopIteration:
+                # Corresponds to the next file.
                 self.file_pointer += 1
-                return self.next()
+                return next(self)
 
         except IndexError:
             raise StopIteration()
@@ -246,9 +250,6 @@ class FileWrapper:
         Return the last line read by this parser.
         """
         return self.last_lines[-1]
-
-    def __next__(self):
-        return self.next()
 
     def __iter__(self):
         return self
