6eb20853c4661266622d23081b8dee87afe95c20
oliver-s-lee <osl@st-andrews.ac.uk>
2024-09-12 11:55:57 +0100

    Added mechanism to test 'parse' PySCF results from py files

---
+++ data/PySCF/basicPySCF2.6/dvb_sp.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2025, the cclib development team
+#
+# This file is part of cclib (http://cclib.github.io) and is distributed under
+# the terms of the BSD 3-Clause License.
+from pyscf import gto, scf
+
+
+def calculate():
+    # This is DVB.
+    mol = gto.Mole(
+        atom="""
+    C          1.43386       -0.00000        0.00000
+    C         -1.43386        0.00000        0.00000
+    C          0.69976       -1.21791        0.00000
+    C         -0.69976        1.21791        0.00000
+    C         -0.70271       -1.21960        0.00000
+    C          0.70271        1.21960        0.00000
+    H          1.24445       -2.17211        0.00000
+    H         -1.24445        2.17211        0.00000
+    H         -1.24514       -2.17312        0.00000
+    H          1.24514        2.17312        0.00000
+    C         -2.93083        0.05118        0.00000
+    C          2.93083       -0.05118        0.00000
+    H         -3.35520        1.06709        0.00000
+    H          3.35520       -1.06709        0.00000
+    C         -3.76833       -1.00028        0.00000
+    C          3.76833        1.00028        0.00000
+    H         -3.41193       -2.03669        0.00000
+    H          3.41193        2.03669        0.00000
+    H         -4.85625       -0.86201        0.00000
+    H          4.85625        0.86201        0.00000
+    """,
+        basis="STO-3G",
+    )
+
+    method = scf.RHF(mol)
+    method.kernel()
+    return method
