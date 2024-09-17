9e497771cc6a2d573fd67c133c6353e2de179ae2
oliver-s-lee <osl@st-andrews.ac.uk>
2024-09-17 12:47:51 +0100

    Fixed PySCF IR test using HF not DFT

--- data/PySCF/basicPySCF2.6/dvb_ir.py
+++ data/PySCF/basicPySCF2.6/dvb_ir.py
@@ -40,7 +40,7 @@ def calculate():
     method.xc = 'b3lyp'
     method.kernel()
     
-    irm = infrared.rhf.Infrared(method)
+    irm = infrared.rks.Infrared(method)
     irm.kernel()
     
     return {
