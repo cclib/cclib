12bf184ea3806eb9eb226ebdae1f9c603da15cc8
Amanda Dumi <amanda.e.dumi@gmail.com>
2024-09-17 22:41:28 -0600

    add framework for parser_metadata

---
+++ cclib/attribute_parsers/parser_metadata.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2025, the cclib development team
+#
+# This file is part of cclib (http://cclib.github.io) and is distributed under
+# the terms of the BSD 3-Clause License.
+from typing import Optional
+
+from cclib.attribute_parsers import utils
+from cclib.attribute_parsers.base_parser import base_parser
+
+import numpy as np
+
+
+class parser_metadata(base_parser):
+    """
+    A temporary variable
+    """
+
+    known_codes = ["psi4"]
+
+    def psi4(file_handler, ccdata):
+        line = file_handler.last_line
+        if "Properties will be evaluated at" in line.strip():
+            if getattr(ccdata, "parser_metadata"):
+                this_metadata = ccdata.parser_metadata
+            else:
+                this_metadata = {}
+            tokens = line.split()
+            assert tokens[-1] in ["Bohr", "[a0]"]
+            this_metadata["origin"] = utils.convertor(
+                np.array([float(x.strip(",")) for x in line.split()[-4:-1]]), "bohr", "Angstrom"
+            )
+            return {parser_metadata.__name__: this_metadata}
+
+    @staticmethod
+    def parse(file_handler, program: str, ccdata) -> Optional[dict]:
+        constructed_data = None
+        if program in parser_metadata.known_codes:
+            file_handler.virtual_set()
+            program_parser = getattr(parser_metadata, program)
+            constructed_data = program_parser(file_handler, ccdata)
+            file_handler.virtual_reset()
+        return constructed_data
