458b05e1fbc58337ff7a6821415e31e0354afed0
oliver-s-lee <osl@st-andrews.ac.uk>
2024-12-10 09:29:56 +0000

    Added tests for Orca 6 NMR, Scan, ROCIS, EOM-CCSD

--- test/data/testTD.py
+++ test/data/testTD.py
@@ -125,7 +125,7 @@ class GenericTDTest:
         "Turbomole/basicTurbomole7.4/CO_adc2_TD",
         "Rotatory strengths are not currently available for ricc2",
     )
-    @skipForLogfile("ORCA/basicORCA6.1/dvb_eom_ccsd.out", "etrotats don't seem to be available for EOM-CCSD")
+    @skipForLogfile("ORCA/basicORCA6.0/dvb_eom_ccsd.log", "etrotats don't seem to be available for EOM-CCSD")
     def testrotatsnumber(self, data) -> None:
         """Is the length of etrotats correct?"""
         assert len(data.etrotats) == self.number
