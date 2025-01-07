f9ce4f14a17f4f49fe2364c4a5208f1a1dadb646
Oliver Lee <oliver.lee2007@gmail.com>
2025-01-07 15:32:47 +0000

    Initial type hints for makecclib function
    Co-authored-by: Eric Berquist <727571+berquist@users.noreply.github.com>

--- cclib/bridge/cclib2pyscf.py
+++ cclib/bridge/cclib2pyscf.py
@@ -7,6 +7,7 @@
 
 import functools
 import itertools
+from typing import Any, Dict, List, Optional, Type
 
 from cclib.parser.data import ccData
 from cclib.parser.utils import PeriodicTable, convertor, find_package
@@ -129,7 +130,13 @@ def makepyscf_mos(ccdata, mol):
     return mo_coeffs, mo_occ, mo_syms, mo_energies
 
 
-def makecclib(*methods, ccsd_t=None, scf_steps=[], opt_steps=[], opt_failed=False) -> ccData:
+def makecclib(
+    *methods: Type[Any],
+    ccsd_t: Optional[float] = None,
+    scf_steps: List[List[Dict[str, float]]] = [],
+    opt_steps: List[Dict[str, Any]] = [],
+    opt_failed: bool = False,
+) -> ccData:
     """Create cclib attributes and return a ccData from a PySCF calculation.
 
     PySCF calculation results are stored in method objects, with each object representing a different part of the
