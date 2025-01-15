841f7015e3a00318ace25261f12073d33723617d
Eric Berquist <eric.berquist@gmail.com>
2025-01-14 22:42:01 -0500

    apply pre-commit fixes

--- test/data/testBOMD.py
+++ test/data/testBOMD.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024, the cclib development team
+# Copyright (c) 2025, the cclib development team
 #
 # This file is part of cclib (http://cclib.github.io) and is distributed under
 # the terms of the BSD 3-Clause License.
