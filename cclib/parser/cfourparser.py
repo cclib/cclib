e2c2e7ee152b977bced387985f183a602ac4d6d0
Slimygen <sslimak67@gmail.com>
2025-01-19 01:48:31 -0500

    Added more attributes to the CFOUR parser

--- cclib/parser/cfourparser.py
+++ cclib/parser/cfourparser.py
@@ -15,46 +15,66 @@ import numpy as np
 
 
 class CFOUR(logfileparser.Logfile):
-    """A CFOUR v2.0 log file."""
+    """A CFOUR v2.1 log file."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(logname="CFOUR", *args, **kwargs)
 
     def __str__(self):
-        """Return a string representation of the object."""
+        #return a string representation of the object
         return f"CFOUR log file {self.filename}"
 
     def __repr__(self):
-        """Return a representation of the object."""
+        #return a representation of the object
         return f'CFOUR("{self.filename}")'
 
     def normalisesym(self, label):
-        """Use standard symmetry labels instead of Gaussian labels.
-
-        To normalise:
-        (1) If label is one of [SG, PI, PHI, DE], replace by [sigma, pi, phi, delta]
-        (2) replace any G or U by their lowercase equivalent
-        """
-        # note: DLT must come after DLTA
-        greek = [("SG", "sigma"), ("PI", "pi"), ("PHI", "phi"), ("DE", "delta")]
-        for k, v in greek:
-            if label.startswith(k):
-                tmp = label[len(k) :]
-                label = v
-                if tmp:
-                    label = f"{v}.{tmp}"
-        ans = label.replace("U", "u").replace("G", "g")
-        return ans
+        pass
 
     def before_parsing(self):
-        pass
+        #set to true so that atomic numbers are parsed on the first block of coordinates
+        self.set_attribute('first_coord_block',True)
+        #set the atomic coordinates to []
+        self.set_attribute('atomcoords',[])
+        #set the list of scf energies to []
+        self.set_attribute('scfenergies',[])
 
     def after_parsing(self):
-        pass
+        #change atomic coordinates to a numpy array
+        self.atomcoords=np.array(self.atomcoords)
+        #change scf energies to a numpy array
+        self.scfenergies=np.array(self.scfenergies)
 
     def extract(self, inputfile, line):
-        if "Version" in line:
-            self.metadata["pavkage_version"]=line.split()[1]
+        #set package metadata to CFOUR
+        self.metadata['package']='CFOUR'
+        #get the version of CFOUR
+        if 'Version' in line:
+            self.metadata['package_version']=line.split()[1]
+        #get the name of the basis set used
+        if 'BASIS                IBASIS' in line:
+            self.metadata['basis_set']=line.split()[2]
+        if 'REFERENCE            IREFNC' in line:
+            self.metadata['unrestricted']=(True if line.split()[2][0]=='U' else False)
+        #get the net charge of the system
+        if 'CHARGE               ICHRGE' in line:
+            self.set_attribute('charge',float(line.split()[2]))
+        #get the spin multiplicity of the system
+        if 'MULTIPLICTY          IMULTP' in line:
+            self.set_attribute('mult',int(line.split()[2]))
+        #get the coordinates at each step in a geometry optimization
+        #if this is the first time parsing a block of coordinates also get the atomic numbers
+        '''
+         ----------------------------------------------------------------
+                     Coordinates used in calculation (QCOMP)
+         ----------------------------------------------------------------
+          Z-matrix   Atomic            Coordinates (in bohr)
+           Symbol    Number           X              Y              Z
+         ----------------------------------------------------------------
+             C         6        -0.00000000     0.00000000     1.23064327
+             O         8         0.00000000     0.00000000    -0.92327590
+         ----------------------------------------------------------------
+        '''
         if 'Coordinates used in calculation (QCOMP)' in line:
             line=next(inputfile)
             line=next(inputfile)
@@ -62,9 +82,19 @@ class CFOUR(logfileparser.Logfile):
             line=next(inputfile)
             line=next(inputfile)
             atomnos=[]
+            temp_atomcoords=[]
             while not '----------------------------------------------------------------' in line:
-                atomnos.append(int(line.strip().split()[1]))
+                if self.first_coord_block:
+                    atomnos.append(int(line.strip().split()[1]))
+                temp_atomcoords.append([float(line.strip().split()[2]),float(line.strip().split()[3]),float(line.strip().split()[4])])
                 line=next(inputfile)
-            self.set_attribute("atomnos",np.array(atomnos))
+            if self.first_coord_block:
+                self.set_attribute('atomnos',atomnos)
+            self.atomcoords.append(temp_atomcoords)
+            self.first_coord_block=False
+        #get the scf energy at each step in a geometry optimization
+        if 'E(SCF)=' in line:
+            self.scfenergies.append(float(line.split()[1]))
+
 
 
