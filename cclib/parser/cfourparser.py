cb6943ee56e020b20917d08e6762c57a6f697573
Slimygen <sslimak67@gmail.com>
2025-02-02 23:19:08 -0500

    Removing CFOUR GeoOpt test due to issue with the output file

--- cclib/parser/cfourparser.py
+++ cclib/parser/cfourparser.py
@@ -181,8 +181,8 @@ class CFOUR(logfileparser.Logfile):
         # get success, cpu time, and wall time
         if "@CHECKOUT-I, Total execution time (CPU/WALL):" in line:
             self.metadata["success"] = True
-            self.metadata["cpu_time"] = timedelta(seconds=float(line.strip().split()[5][:-1]))
-            self.metadata["wall_time"] = timedelta(seconds=float(line.strip().split()[6]))
+            self.metadata["cpu_time"] = [timedelta(seconds=float(line.strip().split()[5][:-1]))]
+            self.metadata["wall_time"] = [timedelta(seconds=float(line.strip().split()[6]))]
         # get the net charge of the system
         if "CHARGE               ICHRGE" in line:
             self.set_attribute("charge", utils.float(line.split()[2]))
