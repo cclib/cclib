1aa3d25fcefbd02f736ffae18ea960a1a72210b1
oliver-s-lee <osl@st-andrews.ac.uk>
2025-02-20 11:51:05 +0000

    Updated Orca performance test for 6.x

--- test/data/testSP.py
+++ test/data/testSP.py
@@ -898,6 +898,13 @@ class ORCAPerformanceMetadataTest:
     def testmetadata_memory_used(self, data) -> None:
         """Does metadata have the expected amount of memory?"""
         assert data.metadata["memory_used"] == 463000000
+        
+class ORCA6PerformanceMetadataTest(ORCAPerformanceMetadataTest):
+    """Check we can parse CPU/memory metadata."""
+
+    def testmetadata_memory_used(self, data) -> None:
+        """Does metadata have the expected amount of memory?"""
+        assert data.metadata["memory_used"] == 22_800_000
 
 
 class TurbomolePerformanceMetadataTest:
