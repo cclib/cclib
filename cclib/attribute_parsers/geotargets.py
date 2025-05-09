57d67b711539cc0fd038a71585b7c1a5f7298b8d
Shiv Upadhyay <shivnupadhyay@gmail.com>
2025-05-09 01:50:46 -0400

    untested geotargets gaussian v2

--- cclib/attribute_parsers/geotargets.py
+++ cclib/attribute_parsers/geotargets.py
@@ -14,7 +14,7 @@ class geotargets(base_parser):
     Docstring? Units?
     """
 
-    known_codes = ["psi4"]
+    known_codes = ["psi4", "gaussian"]
 
     @staticmethod
     def psi4(file_handler, ccdata) -> Optional[dict]:
@@ -79,7 +79,34 @@ class geotargets(base_parser):
             return {geotargets.__name__: this_scftargets}
         return None
 
-        # This section prints contraction information before the atomic basis set functions and
+    @staticmethod
+    def gaussian(file_handler, ccdata) -> Optional[dict]:
+        line = file_handler.last_line
+        # Geometry convergence information.
+        if line[49:59] == "Converged?":
+            parsed_geotargets = []
+            if not hasattr(ccdata, "geotargets"):
+                parsed_geotargets = numpy.array([0.0, 0.0, 0.0, 0.0], "d")
+            allconverged = True
+            newlist = [0] * 4
+            for i in range(4):
+                line = file_handler.virtual_next()
+                parts = line.split()
+                if "NO" in parts[-1]:
+                    allconverged = False  # noqa: F841
+                try:
+                    value = utils.float(parts[2])
+                except ValueError:
+                    pass # todo logging
+                    #self.logger.error(
+                    #    "Problem parsing the value for geometry optimisation: %s is not a number.",
+                    #    parts[2],
+                    #)
+                else:
+                    newlist[i] = value
+                parsed_geotargets[i] = utils.float(parts[3])
+            return {geotargets.__name__: parsed_geotargets}
+        return None
 
     @staticmethod
     def parse(file_handler, program, ccdata) -> Optional[dict]:
