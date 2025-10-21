abee16c9322f7686640e13b5af507494eeefaaff
Lukas Paetow <71271093+lukas-paetow@users.noreply.github.com>
2025-10-22 00:50:22 +0200

    Serenity: SCF targets (#1668)

--- cclib/parser/serenityparser.py
+++ cclib/parser/serenityparser.py
@@ -66,6 +66,18 @@ class Serenity(logfileparser.Logfile):
         if line[5:21] == "Basis Functions:":
             self.set_attribute("nbasis", int(line.split()[2]))
 
+        # Extract SCF thresholds
+        if line.strip().startswith("Energy Threshold:"):
+            scftargets = []
+            ethresh = float(line.split()[2])
+            line = next(inputfile)
+            if "RMSD[D]" in line:
+                rmsd = float(line.split()[2])
+                line = next(inputfile)
+                if "DIIS" in line:
+                    diis = float(line.split()[2])
+                    scftargets.append(numpy.array([ethresh, rmsd, diis]))
+                    self.set_attribute("scftargets", scftargets)
         if "Total Energy" in line:
             self.append_attribute("scfenergies", float(line.split()[3]))
 
