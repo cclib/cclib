c767e17863e258206361e946965f835b6a5de54f
Michael Marshall <physbean@gmail.com>
2026-07-25 21:15:18 -0400

    Use cclib.exceptions.MissingAttributeError instead of local class (refs #1746)

--- cclib/bridge/cclib2pyscf.py
+++ cclib/bridge/cclib2pyscf.py
@@ -20,8 +20,7 @@ import periodictable
 l_sym2num = {"S": 0, "P": 1, "D": 2, "F": 3, "G": 4}
 
 
-class MissingAttributeError(Exception):
-    pass
+from cclib.exceptions import MissingAttributeError
 
 
 _found_pyscf = find_package("pyscf")
